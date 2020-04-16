88c17d0a358d08cd1d44c8eee5d808efdc645aa7
Øystein Haug Olsen <oystein.olsen@cognite.com>
2020-04-16 20:34:20 +0200

    Add Chunking

---
+++ ExtractorUtils.Test/ChunkingTest.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using ExtractorUtils;
+
+namespace ExtractorUtils.Test {
+
+    public static class ChunkingTest {
+
+        [Fact]
+        public static void RunThrottledOK()
+        {
+            var completed = new List<int>();
+            var token = CancellationToken.None;
+            var generators = Enumerable.Range(1, 5).Select<int, Func<Task>>(
+                i => async () => {
+                    Console.Out.WriteLine($"Starting {i}");
+                    await Task.Delay(i * 100, token);
+                    Console.Out.WriteLine($"Completed {i}");
+                    completed.Add(i);
+                });
+            Task.WhenAll(generators.RunThrottled(2, token)).GetAwaiter().GetResult();
+            Assert.Equal(5, completed.Count);
+            Assert.Equal(15, completed.Sum());
+        }
+
+        [Fact]
+        public static void RunThrottledException()
+        {
+            var completed = new List<int>();
+            var token = CancellationToken.None;
+            var generators = Enumerable.Range(1, 5).Select<int, Func<Task>>(
+                i => async () => {
+                    Console.Out.WriteLine($"Starting {i}");
+                    await Task.Delay(i * 100, token);
+                    if (i == 3)
+                    {
+                        throw new Exception("Failed on 3!!!");
+                    }
+                    Console.Out.WriteLine($"Completed {i}");
+                    completed.Add(i);
+                });
+
+            Assert.Throws<Exception>(
+                delegate {
+                    Task.WhenAll(generators.RunThrottled(2, token)).GetAwaiter().GetResult();
+                    Thread.Sleep(2000);
+                }
+            );
+            Assert.Equal(2, completed.Count);
+            Assert.Equal(3, completed.Sum());
+        }
+
+        private static IEnumerable<(string id, double value)> DataPoints(int timeseries, int datapoints)
+        {
+            for (int i = 0; i < timeseries; i++)
+            {
+                for (int j = 0; j < datapoints; j++)
+                {
+                    yield return ($"id{i}", j);
+                }
+            }
+        }
+        
+        [Theory]
+        [InlineData(100_000, 10_000, 20000, 100, 20, 1000, 100000)]
+        [InlineData(100_000, 10_000, 200, 10000, 20, 10, 100000)]
+        [InlineData(100_000, 10_000, 20000, 5, 2, 10000, 50000)]
+        [InlineData(100, 10_000, 1, 10_000, 100, 1, 100)]
+        public static void TestDictionaryChunking(
+            int dpChunk, int tsChunk,
+            int timeseries, int datapoints,
+            int expChunks, int expTimeseriesMax, int expDatapointsMax)
+        {
+            var dict = new List<(string, IEnumerable<int>)>();
+            for (int i = 0; i < timeseries; i++)
+            {
+                var points = new List<int>();
+                for (int j = 0; j < datapoints; j++)
+                {
+                    points.Add(i * datapoints + j);
+                }
+                dict.Add(($"id{i}", points));
+            }
+            var results = dict.ChunkBy(dpChunk, tsChunk);
+            var min = results.Min(c => c.Min(e => e.Values.Count()));
+            Assert.True(min > 0);
+            var max = results.Max(c => c.Select(e => e.Values.Count()).Sum());
+            var maxTs = results.Max(x => x.Count());
+            Assert.Equal(expDatapointsMax, max);
+            Assert.Equal(expTimeseriesMax, maxTs);
+            Assert.Equal(expChunks, results.Count());
+            //var total = results.Sum(dct => dct.Values.Sum(val => val.Count()));
+            var total = results.SelectMany(x => x.Select(y => y.Values.Count())).Sum();
+            var totalTs = results.SelectMany(x => x.Select(y => y.Key)).ToHashSet().Count;
+            Assert.Equal(timeseries, totalTs);
+            Assert.Equal(datapoints * timeseries, total);
+
+            var exists = new bool[timeseries * datapoints];
+            foreach (var chunk in results)
+            {
+                foreach (var (id, values) in chunk)
+                {
+                    foreach (var value in values)
+                    {
+                        exists[value] = true;
+                    }
+                }
+            }
+            Assert.True(exists.All(val => val));
+        }
+
+    }
+}
---
+++ ExtractorUtils/Utils/Chunking.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ExtractorUtils
+{
+    static public class Chunking
+    {
+        public static IEnumerable<IEnumerable<T>> Chunked<T>(this IEnumerable<T> things, int itemsPerChunk)
+        {
+            var list = new List<T>(itemsPerChunk);
+            foreach (var thing in things)
+            {
+                list.Add(thing);
+                if (list.Count == itemsPerChunk)
+                {
+                    yield return list;
+                    list = new List<T>(itemsPerChunk);
+                }
+            }
+
+            if (list.Any())
+            {
+                yield return list;
+            }
+        }
+
+        public static IEnumerable<IEnumerable<(TKey Key, IEnumerable<TVal> Values)>> ChunkBy<TKey, TVal>(
+            this IEnumerable<(TKey, IEnumerable<TVal>)> points, int maxPerList, int maxKeys)
+        {
+            var current = new List<(TKey, IEnumerable<TVal>)>();
+            int count = 0;
+
+            foreach (var (key, values) in points)
+            {
+                if (!values.Any())
+                    continue;
+
+                if (current.Count >= maxKeys)
+                {
+                    yield return current;
+                    current = new List<(TKey, IEnumerable<TVal>)>();
+                    count = 0;
+                }
+
+                int pcount = values.Count();
+                if (count + pcount <= maxPerList)
+                {
+                    current.Add((key, values));
+                    count += pcount;
+                    continue;
+                }
+
+                // fill up the current batch to max_datapoints data points and keep the remaining data points in current.
+                var inCurrent = values.Take(Math.Min(maxPerList - count, pcount)).ToList();
+                if (inCurrent.Count > 0)
+                {
+                    current.Add((key, inCurrent));
+                }
+                yield return current;
+                current = null;
+
+                // inNext can have too many datapoints
+                var inNext = values.Skip(inCurrent.Count);
+                if (inNext.Any())
+                {
+                    var chunks = ChunkBy(inNext, maxPerList).Select(chunk => (Key: key, Values: chunk));
+                    foreach (var chunk in chunks)
+                    {
+                        if (chunk.Values.Count() == maxPerList)
+                        {
+                            yield return new List<(TKey, IEnumerable<TVal>)> { chunk };
+                            continue;
+                        }
+                        if (chunk.Values.Any())
+                        {
+                            count = chunk.Values.Count();
+                            current = new List<(TKey, IEnumerable<TVal>)> { chunk };
+                        }
+                        break;
+                    }
+                }
+                if (current == null)
+                {
+                    current = new List<(TKey, IEnumerable<TVal>)>();
+                    count = 0;
+                }
+            }
+
+            if (current.Any())
+            {
+                yield return current;
+            }
+        }
+
+        public static IEnumerable<IEnumerable<T>> ChunkBy<T>(IEnumerable<T> input, int maxSize)
+        {
+            return input
+                .Select((x, i) => new { Index = i, Value = x })
+                .GroupBy(x => x.Index / maxSize)
+                .Select(x => x.Select(v => v.Value));
+        }
+
+        private static List<T> Dequeue<T>(this Queue<T> queue, int numToDequeue)
+        {
+            List<T> ret = new List<T>();
+            while (queue.Any() && ret.Count < numToDequeue)
+            {
+                ret.Add(queue.Dequeue());
+            }
+            return ret;
+        }
+
+        public static async Task RunThrottled(
+            this IEnumerable<Func<Task>> generators,
+            int limit,
+            CancellationToken token)
+        {
+            List<Task> tasks = new List<Task>();
+            var generatorQueue = new Queue<Func<Task>>(generators);
+            int totalTasks = generatorQueue.Count;
+            int completedTasks = 0;
+            while (generatorQueue.Any() || tasks.Any())
+            {
+                token.ThrowIfCancellationRequested();
+                if (tasks.Any())
+                {
+                    var task = await Task.WhenAny(tasks);
+                    // will throw exception if the task failed, returns immediately
+                    await task;
+                    completedTasks++;
+                    Debug.Assert(completedTasks <= totalTasks);
+                    tasks.Remove(task);
+                }
+
+                Debug.Assert(tasks.Count < limit);
+                if (generatorQueue.Any())
+                {
+                    int toInsert = limit - tasks.Count;
+                    tasks.AddRange(generatorQueue.Dequeue(toInsert).Select(gen => gen()));
+                }
+            }
+        }
+
+    }
+}
