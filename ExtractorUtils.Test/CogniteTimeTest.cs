07e2220bbe63eecade3dae4d1c682b78cdf65d46
Øystein Haug Olsen <oystein.olsen@cognite.com>
2020-04-16 20:00:10 +0200

    Add CogniteTime (copied from PI UnixTime)
    Renamed ms since epoch

---
+++ ExtractorUtils.Test/CogniteTimeTest.cs
@@ -0,0 +1,80 @@
+using Xunit;
+using System;
+
+namespace ExtractorUtils.Test
+{
+    public static class CogniteTimeTest
+    {
+
+        [Theory]
+        [InlineData(1573627744000, "2019-11-13 06:49:04.000")]
+        public static void TestToFormattedString(
+            long ms, string expected)
+        {
+            var time = CogniteTime.FromMilliseconds(ms);
+            Assert.Equal(expected, time.ToISOString());
+        }
+
+        [Fact]
+        public static void TestEpochToString()
+        {
+            Assert.Equal("1970-01-01 00:00:00.000", CogniteTime.DateTimeEpoch.ToISOString());
+        }
+
+        [Fact]
+        public static void TestMaxValueToString()
+        {
+            Assert.Equal("9999-12-31 23:59:59.999", DateTime.MaxValue.ToISOString());
+        }
+
+        [Fact]
+        public static void TestMax()
+        {
+            var t1 = CogniteTime.FromMilliseconds(1000);
+            var t2 = CogniteTime.FromMilliseconds(2000);
+            Assert.Equal(t2, CogniteTime.Max(t1, t2));
+        }
+
+        [Fact]
+        public static void TestMin()
+        {
+            var t1 = CogniteTime.FromMilliseconds(1000);
+            var t2 = CogniteTime.FromMilliseconds(2000);
+            Assert.Equal(t1, CogniteTime.Min(t1, t2));
+        }
+        [Fact]
+        public static void TestMaxValue()
+        {
+            var msMax = DateTime.MaxValue.MillisecondsSinceEpoch();
+            var dMax = CogniteTime.FromMilliseconds(msMax);
+            Assert.Equal("9999-12-31 23:59:59.999", dMax.ToISOString());
+            Assert.Equal(DateTime.MaxValue.Ticks / 10_000, dMax.Ticks / 10_000);
+        }
+
+        [Fact]
+        public static void TestFromMilliseconds()
+        {
+            var d1 = CogniteTime.FromMilliseconds(0);
+            Assert.Equal(CogniteTime.DateTimeEpoch, d1);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => CogniteTime.FromMilliseconds(-1));
+
+            var outOfRangeValue = DateTime.MaxValue.MillisecondsSinceEpoch() + 1;
+            Assert.Throws<ArgumentOutOfRangeException>(() => CogniteTime.FromMilliseconds(outOfRangeValue));
+        }
+
+        [Fact]
+        public static void TestNanosecondsSinceEpoch()
+        {
+            // Maximum date that can be stored as (long) nanoseconds since epoch.
+            var valid = DateTime.Parse("2262-04-11T23:47:16.8547758Z").ToUniversalTime();
+            var invalid1 = valid.AddTicks(1);
+            var invalid2 = new DateTime(2262, 04, 11, 23, 47, 16, 855, DateTimeKind.Utc);
+            var ns = CogniteTime.NanosecondsSinceEpoch(valid);
+            Assert.Equal(CogniteTime.FromTicks(ns / 100), valid);
+            Assert.Throws<ArgumentException>(() => CogniteTime.NanosecondsSinceEpoch(invalid1));
+            Assert.Throws<ArgumentException>(() => CogniteTime.NanosecondsSinceEpoch(invalid2));
+        }
+
+    }
+}
---
+++ ExtractorUtils/Utils/CogniteTime.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace ExtractorUtils
+{
+    public static class CogniteTime
+    {
+        /// <summary>
+        /// DateTime object representing the Unix Epoch.
+        /// </summary>
+        public static DateTime DateTimeEpoch => new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        
+        private static readonly long epochTicks = DateTimeEpoch.Ticks;
+        private static readonly long maxTsValue = DateTime.MaxValue.MillisecondsSinceEpoch();
+        private static readonly long maxTicksValue = DateTime.MaxValue.TicksSinceEpoch();
+
+        /// <summary>
+        /// Constructs a DateTime object adding the number of milliseconds passed as parameter to
+        /// Unix Epoch.
+        /// </summary>
+        /// <param name="msSinceEpoch">number of milliseconds since Epoch</param>
+        /// <returns>DateTime object correponding to the Unix time</returns>
+        public static DateTime FromMilliseconds(long msSinceEpoch)
+        {
+            if (msSinceEpoch < 0 || msSinceEpoch > maxTsValue)
+            {
+                throw new ArgumentOutOfRangeException(nameof(msSinceEpoch), $"Timestamp value should be between {0} and {maxTsValue} ms");
+            }
+            return DateTimeEpoch.AddMilliseconds(msSinceEpoch);
+        }
+
+        /// <summary>
+        /// Constructs a DateTime object adding the number of ticks passed as parameter to
+        /// Unix Epoch.
+        /// </summary>
+        /// <param name="ticksSinceEpoch">number of ticks since Epoch</param>
+        /// <returns>DateTime object correponding to the Unix time</returns>
+        public static DateTime FromTicks(long ticksSinceEpoch)
+        {
+            if (ticksSinceEpoch < 0 || ticksSinceEpoch > maxTicksValue)
+            {
+                throw new ArgumentOutOfRangeException(nameof(ticksSinceEpoch), $"Timestamp value should be between {0} and {maxTsValue} ticks");
+            }
+            return DateTimeEpoch.AddTicks(ticksSinceEpoch);
+        }
+
+        /// <summary>
+        /// Returns the how many milliseconds have passed since Unix Epoch to the
+        /// date passed as parameter.
+        /// NOTE: Using TimeSpan.ToMilliseconds may cause rounding problems when 'time' is DateTime.MaxValue:
+        /// The resulting(time - DateTimeEpoch).ToMilliseconds is 253402300800000, which cannot be
+        /// converted back to DateTime(ArgumentOutOfRangeException). This method returns 253402300799999 instead.
+        /// </summary>
+        /// <param name="time">DateTime object to convert</param>
+        /// <returns>Number of milliseconds since Epoch</returns>
+        public static long MillisecondsSinceEpoch(this DateTime time)
+        {
+            var timestamp = TicksSinceEpoch(time) / TimeSpan.TicksPerMillisecond;
+            return timestamp;
+        }
+
+        /// <summary>
+        /// Returns the how many ticks have passed since Unix Epoch to the
+        /// date passed as parameter. A Tick correspond to 10.000 ms (ref. TimeSpan.TicksPerMillisecond).
+        /// </summary>
+        /// <param name="time">DateTime object to convert</param>
+        /// <returns>Number of ticks since Epoch</returns>
+        public static long TicksSinceEpoch(this DateTime time)
+        {
+            if (time < DateTimeEpoch)
+            {
+                throw new ArgumentException($"Date {time.ToISOString()} is before Unix Epoch.");
+            }
+            if (time.Kind == DateTimeKind.Local)
+            {
+                throw new ArgumentException("DateTime object should be represented using UTC");
+            }
+            return time.Ticks - epochTicks;
+        }
+
+        /// <summary>
+        /// Returns the how many nanoseconds have passed since Unix Epoch to the
+        /// date passed as parameter. A Nanosecond correspond to 100 ticks.
+        /// </summary>
+        /// <param name="time">DateTime object to convert</param>
+        /// <returns>Number of ticks since Epoch</returns>
+        public static long NanosecondsSinceEpoch(this DateTime time)
+        {
+            var timestamp = TicksSinceEpoch(time);
+            if (timestamp > long.MaxValue / 100)
+            {
+                throw new ArgumentException("Maxim value for nanosecond timestamp exceeded");
+            }
+            return timestamp * 100;
+        }
+
+        // Return ISO 8601 formatted string
+        public static string ToISOString(this DateTime time)
+        {
+            return time.ToString("yyyy-MM-dd HH\\:mm\\:ss.fff");
+        }
+
+        public static DateTime Max(DateTime t1, DateTime t2)
+        {
+            return t1 > t2 ? t1 : t2;
+        }
+
+        public static DateTime Min(DateTime t1, DateTime t2)
+        {
+            return t1 < t2 ? t1 : t2;
+        }
+
+    }
+}
