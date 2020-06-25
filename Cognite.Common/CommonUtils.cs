57e4a2f9341f37d8aabaeefa781609afd8e10d8d
Einar Omang <einar@omang.com>
2020-06-25 09:19:44 +0200

    Some more utils
    Min/max timestamp from list
    
    Group by time granularity
    
    DistinctBy

--- Cognite.Common/CommonUtils.cs
+++ Cognite.Common/CommonUtils.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Cognite.Extractor.Common
 {
@@ -28,6 +30,71 @@ namespace Cognite.Extractor.Common
         public static TValue GetValue<TKey,TValue>(this IDictionary<TKey, TValue> dict, TKey key, TValue defaultValue)
         {
             return dict.TryGetValue(key, out TValue value) ? value : defaultValue;
+        }
+
+        /// <summary>
+        /// Get the minimum and maximum timestamp in a list, using <paramref name="tsSelector"/> to get the timestamp for
+        /// each item
+        /// </summary>
+        /// <typeparam name="T">Input element type</typeparam>
+        /// <param name="items">Input enumerable</param>
+        /// <param name="tsSelector">Function to get timestamp for each element</param>
+        /// <returns>A tuple (Min, Max)</returns>
+        public static (DateTime Min, DateTime Max) MinMax<T>(this IEnumerable<T> items, Func<T, DateTime> tsSelector)
+        {
+            DateTime min = DateTime.MaxValue;
+            DateTime max = DateTime.MinValue;
+            foreach (var item in items)
+            {
+                DateTime ts = tsSelector(item);
+                if (ts < min) min = ts;
+                if (ts > max) max = ts;
+            }
+            return (min, max);
+        }
+
+        /// <summary>
+        /// Group input by some granularity, so that the result is a list of lists where
+        /// each inner list contains only elements within <paramref name="granularity"/> time of each other.
+        /// </summary>
+        /// <typeparam name="T">Input element type</typeparam>
+        /// <param name="items">Input enumerable</param>
+        /// <param name="granularity">Granularity to chunk by</param>
+        /// <param name="tsSelector">Function to get timestamp for each element</param>
+        /// <param name="maxLength">Maximum number of elements in each inner list</param>
+        /// <returns>A list of lists where each inner list contains only elements within <paramref name="granularity"/>
+        /// time of each other.</returns>
+        public static IEnumerable<IEnumerable<T>> GroupByTimeGranularity<T>(
+            this IEnumerable<T> items,
+            TimeSpan granularity,
+            Func<T, DateTime> tsSelector,
+            int maxLength)
+        {
+            return granularity == TimeSpan.Zero
+                ? items.Select(item => new[] { item })
+                : items.GroupBy(item => tsSelector(item).Ticks / granularity.Ticks)
+                    .SelectMany(group => group.ChunkBy(maxLength));
+        }
+
+        /// <summary>
+        /// Returns elements of the source enumerable, where all elments have distinct results of the selector function.
+        /// </summary>
+        /// <param name="source">Input enumerable</param>
+        /// <param name="selector">Function to generate keys</param>
+        /// <param name="comparer">Optional element comparer</param>
+        /// <returns></returns>
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source,
+            Func<TSource, TKey> selector, IEqualityComparer<TKey> comparer = null)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            HashSet<TKey> seenKeys = new HashSet<TKey>(comparer);
+            foreach (var elem in source)
+            {
+                if (seenKeys.Add(selector(elem)))
+                {
+                    yield return elem;
+                }
+            }
         }
     }
 }
