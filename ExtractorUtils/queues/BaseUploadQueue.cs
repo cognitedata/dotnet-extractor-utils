eef9466fce5cb5fc64bf16d85698da244ae51322
Einar Omang <einar@omang.com>
2020-07-15 13:13:35 +0200

    Create base upload queue
    Refactor RawUploadQueue to use the base queue. Most of the queue
    functionality is implemented in the base queue, only specific
    functionality should be implemented in the derived queues.

---
+++ ExtractorUtils/queues/BaseUploadQueue.cs
@@ -0,0 +1,218 @@
+﻿using CogniteSdk;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cognite.Extractor.Utils
+{
+    /// <summary>
+    /// Result of an attempt to upload from an upload queue.
+    /// </summary>
+    /// <typeparam name="T">Queue item type</typeparam>
+    public class QueueUploadResult<T>
+    {
+        /// <summary>
+        /// List of items to be uploaded, may be null if upload failed, or empty if no objects were uploaded.
+        /// </summary>
+        public IEnumerable<T> Uploaded { get; }
+        /// <summary>
+        /// True if upload failed completely.
+        /// </summary>
+        public bool IsFailed => Exception != null;
+        /// <summary>
+        /// Exception if upload failed completely.
+        /// </summary>
+        public Exception Exception { get; }
+        /// <summary>
+        /// Constructor for successfull or empty upload.
+        /// </summary>
+        /// <param name="uploaded"></param>
+        public QueueUploadResult(IEnumerable<T> uploaded)
+        {
+            Uploaded = uploaded;
+        }
+        /// <summary>
+        /// Constructor for failed upload.
+        /// </summary>
+        /// <param name="ex">Fatal exception</param>
+        public QueueUploadResult(Exception ex)
+        {
+            Exception = ex;
+        }
+    }
+
+    /// <summary>
+    /// Generic base class for upload queues
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class BaseUploadQueue<T> : IDisposable
+    {
+        private readonly int _maxSize;
+        /// <summary>
+        /// CogniteDestination to use
+        /// </summary>
+        protected readonly CogniteDestination _destination;
+        /// <summary>
+        /// Callback on upload
+        /// </summary>
+        protected readonly Func<QueueUploadResult<T>, Task> _callback;
+
+        private readonly ConcurrentQueue<T> _items;
+        /// <summary>
+        /// Logger to use
+        /// </summary>
+        protected readonly ILogger<CogniteDestination> _logger;
+        private readonly ManualResetEventSlim _pushEvent;
+        private readonly System.Timers.Timer _timer;
+
+        private CancellationTokenSource _tokenSource;
+        private Task _uploadTask;
+
+        internal BaseUploadQueue(
+            CogniteDestination destination,
+            TimeSpan interval,
+            int maxSize,
+            ILogger<CogniteDestination> logger)
+        {
+            _maxSize = maxSize;
+            _destination = destination;
+            _items = new ConcurrentQueue<T>();
+            _logger = logger;
+            _pushEvent = new ManualResetEventSlim(false);
+
+            if (interval == TimeSpan.Zero || interval == Timeout.InfiniteTimeSpan) return;
+
+            _timer = new System.Timers.Timer
+            {
+                Interval = interval.TotalMilliseconds,
+                AutoReset = false
+            };
+            _timer.Elapsed += (sender, e) => _pushEvent.Set();
+        }
+
+        /// <summary>
+        /// Enqueue a single item in the internal queue.
+        /// </summary>
+        /// <param name="item">Item to enqueue</param>
+        public void Enqueue(T item)
+        {
+            _items.Enqueue(item);
+            if (_maxSize > 0 && _items.Count >= _maxSize)
+            {
+                _pushEvent.Set();
+            }
+        }
+
+        /// <summary>
+        /// Empty the queue and return the contents
+        /// </summary>
+        /// <returns>Contents of the queue</returns>
+        public IEnumerable<T> Dequeue()
+        {
+            var items = new List<T>();
+            while (_items.TryDequeue(out T item))
+            {
+                items.Add(item);
+            }
+            return items;
+        }
+
+        /// <summary>
+        /// Trigger queue upload and return the result instead of calling the callback.
+        /// Can be used as alternative for callback entirely.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns>A <see cref="QueueUploadResult{T}"/> containing an error or the uploaded entries</returns>
+        public async Task<QueueUploadResult<T>> Trigger(CancellationToken token)
+        {
+            var items = Dequeue();
+            return await UploadEntries(items, token);
+        }
+
+        /// <summary>
+        /// Start automatically uploading queue entries
+        /// </summary>
+        public async Task Start(CancellationToken token)
+        {
+            _tokenSource = CancellationTokenSource.CreateLinkedTokenSource(token);
+            _timer.Start();
+            _logger.LogDebug("Queue of type {Type} started", GetType().Name);
+            _uploadTask = Task.Run(async () =>
+            {
+                try
+                {
+                    while (!_tokenSource.IsCancellationRequested)
+                    {
+                        _pushEvent.Wait(_tokenSource.Token);
+                        var result = await Trigger(_tokenSource.Token);
+                        _pushEvent.Reset();
+                        _timer.Start();
+                        if (_callback != null) await _callback(result);
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    // ignore cancel exceptions, but throw any other exception
+                    _logger.LogDebug("Upload queue of type {Type} cancelled with {QueueSize} items left", GetType().Name, _items.Count);
+                }
+            });
+            await _uploadTask;
+        }
+        /// <summary>
+        /// Method called to upload entries
+        /// </summary>
+        /// <param name="items">Items to upload</param>
+        /// <param name="token"></param>
+        /// <returns>A <see cref="QueueUploadResult{T}"/> containing an error or the uploaded entries</returns>
+        protected abstract Task<QueueUploadResult<T>> UploadEntries(IEnumerable<T> items, CancellationToken token);
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        /// <summary>
+        /// Dispose of the queue, uploading all remaining entries.
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    try
+                    {
+                        _timer.Stop();
+                        var items = Dequeue();
+                        var result = UploadEntries(items, _tokenSource.Token).GetAwaiter().GetResult();
+                        if (_callback != null) _callback(result).GetAwaiter().GetResult();
+                        if (!_tokenSource.IsCancellationRequested)
+                        {
+                            _tokenSource.Cancel();
+                        }
+                        _uploadTask.GetAwaiter().GetResult();
+                    }
+                    finally
+                    {
+                        _pushEvent.Dispose();
+                        _timer.Close();
+                        _tokenSource.Dispose();
+                    }
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        /// <summary>
+        /// Dispose of the queue, uploading all remaining entries.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+    }
+}
---
+++ ExtractorUtils/queues/RawUploadQueue.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Extensions.Logging;
+using Prometheus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cognite.Extractor.Utils
+{
+    /// <summary>
+    /// Interface to CDF Raw upload queues. The items in the queue are
+    /// DTOs (data type objects) of type <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IRawUploadQueue<T> : IDisposable
+    {
+        /// <summary>
+        /// Trigger upload immediately, returning upload result
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns>Wrapper containing the list of columns or an error if upload failed</returns>
+        Task<QueueUploadResult<(string key, T columns)>> Trigger(CancellationToken token);
+        /// <summary>
+        /// Enqueue the DTO of type <typeparamref name="T"/> to be uploaded
+        /// as a row to CDF Raw.
+        /// </summary>
+        /// <param name="key">The row key</param>
+        /// <param name="columns">The row columns</param>
+        void EnqueueRow(string key, T columns);
+
+        /// <summary>
+        /// Starts a <see cref="Task"/> to insert rows into CDF Raw at regular intervals. Waiting on the returned 
+        /// task will throw any eventual exceptions. To stop the upload queue, dispose the upload queue object or
+        /// cancel the provided <paramref name="token"/>.
+        /// </summary>
+        /// <param name="token">Cancellation token</param>
+        /// <returns>Upload queue task</returns>
+        Task Start(CancellationToken token);
+    }
+    class RawUploadQueue<T> : BaseUploadQueue<(string key, T columns)>, IRawUploadQueue<T>
+    {
+        private readonly string _db;
+        private readonly string _table;
+
+        private static readonly Counter _numberRows = Prometheus.Metrics.CreateCounter("extractor_utils_raw_rows",
+            "Number of rows uploaded to CDF Raw", "type");
+        private static readonly Gauge _queueSize = Prometheus.Metrics.CreateGauge("extractor_utils_raw_queue_size",
+            "Number of elements in the upload queue to CDF Raw", "type");
+
+        internal RawUploadQueue(
+            string db,
+            string table,
+            CogniteDestination destination,
+            TimeSpan interval,
+            int maxSize,
+            ILogger<CogniteDestination> logger) : base(destination, interval, maxSize, logger)
+        {
+            _db = db;
+            _table = table;
+        }
+
+        public void EnqueueRow(string key, T columns)
+        {
+            Enqueue((key, columns));
+            _queueSize.WithLabels(typeof(T).Name).Inc();
+        }
+
+        protected override async Task<QueueUploadResult<(string key, T columns)>> UploadEntries(
+            IEnumerable<(string key, T columns)> items, CancellationToken token)
+        {
+            var rows = items.ToDictionary(pair => pair.key, pair => pair.columns);
+            if (!rows.Any())
+            {
+                return new QueueUploadResult<(string key, T columns)>(Enumerable.Empty<(string key, T columns)>());
+            }
+            _logger.LogTrace("Dequeued {Number} {Type} rows to upload to CDF Raw", rows.Count, typeof(T).Name);
+            try
+            {
+                await _destination.InsertRawRowsAsync(_db, _table, rows, token);
+            }
+            catch (Exception ex)
+            {
+                return new QueueUploadResult<(string key, T columns)>(ex);
+            }
+            _numberRows.WithLabels(typeof(T).Name).Inc(rows.Count);
+            return new QueueUploadResult<(string key, T columns)>(items);
+        }
+    }
+}
