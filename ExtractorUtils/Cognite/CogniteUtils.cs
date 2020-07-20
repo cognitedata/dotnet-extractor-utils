c9b26a1ce7cc363d3d7ff21206f2f88d53087aad
Einar Marstrander Omang <einar.omang@cognite.com>
2020-07-20 11:38:52 +0200

    Binary buffer (#55)
    Methods to serialize and deserialize datapoints and events to a simple binary format. Using a fixed format like this makes it very compact and fast. Deserialization uses a stream for efficiency. Points are stored as [id][point][point]..., so ids may be duplicated in a file. This is not a very stable format, and it may not work well if moved between systems.
    
    Strings are encoded as UTF-8, and are prefixed by their length.

--- ExtractorUtils/Cognite/CogniteUtils.cs
+++ ExtractorUtils/Cognite/CogniteUtils.cs
@@ -12,6 +12,8 @@ using Polly.Timeout;
 using Cognite.Extractor.Common;
 using System.Linq;
 using Cognite.Common;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Cognite.Extractor.Utils
 {
@@ -161,6 +163,354 @@ namespace Cognite.Extractor.Utils
         public static IEnumerable<Datapoint> RemoveOutOfRangeTimestamps(this IEnumerable<Datapoint> points)
         {
             return points.Where(p => p.Timestamp >= TimestampMin && p.Timestamp <= TimestampMax);
+        }
+        /// <summary>
+        /// Turn string into an array of bytes on the format [unsigned short length][string].
+        /// </summary>
+        /// <param name="str">String to transform</param>
+        /// <returns>Storable bytes</returns>
+        public static byte[] StringToStorable(string str)
+        {
+            if (str == null)
+            {
+                return BitConverter.GetBytes((ushort)0);
+            }
+            var strBytes = System.Text.Encoding.UTF8.GetBytes(str);
+            ushort size = (ushort)strBytes.Length;
+            byte[] bytes = new byte[size + sizeof(ushort)];
+            Buffer.BlockCopy(BitConverter.GetBytes(size), 0, bytes, 0, sizeof(ushort));
+            if (size == 0) return bytes;
+            Buffer.BlockCopy(strBytes, 0, bytes, sizeof(ushort), size);
+            return bytes;
+        }
+        /// <summary>
+        /// Read a string from given array of bytes, starting from given position.
+        /// String being read is assumed to be on the format [unsigned short length][string]
+        /// </summary>
+        /// <param name="stream">Stream to read from</param>
+        /// <param name="size">Optional size, if this is non-null, skip reading size from the stream</param>
+        /// <returns>Resulting parsed string</returns>
+        public static string StringFromStream(Stream stream, ushort? size = null)
+        {
+            if (!size.HasValue)
+            {
+                var sizeBytes = new byte[sizeof(ushort)];
+                if (stream.Read(sizeBytes, 0, sizeof(ushort)) < sizeof(ushort)) return null;
+                size = BitConverter.ToUInt16(sizeBytes, 0);
+                if (size == 0) return null;
+            }
+
+            var bytes = new byte[size.Value];
+            if (stream.Read(bytes, 0, size.Value) < size.Value) return null;
+
+            return System.Text.Encoding.UTF8.GetString(bytes);
+        }
+
+        /// <summary>
+        /// Write dictionary of datapoints to a stream.
+        /// Encoding for one timeseries:
+        /// [ushort id-length, 0 if internalId]{Either [long internalId] or [ushort size][string]}[uint count][dp 1]...[dp count]
+        /// </summary>
+        /// <param name="datapoints">Datapoints to store</param>
+        /// <param name="stream">Stream to write to</param>
+        /// <param name="token"></param>
+        public static Task WriteDatapointsAsync(IDictionary<Identity, IEnumerable<Datapoint>> datapoints, Stream stream, CancellationToken token)
+        {
+            return Task.Run(() => WriteDatapoints(datapoints, stream), token);
+        }
+
+        /// <summary>
+        /// Write dictionary of datapoints to a stream.
+        /// Encoding for one timeseries:
+        /// [ushort id-length, 0 if internalId]{Either [long internalId] or [ushort size][string]}[uint count][dp 1]...[dp count]
+        /// </summary>
+        /// <param name="datapoints">Datapoints to store</param>
+        /// <param name="stream">Stream to write to</param>
+        public static void WriteDatapoints(IDictionary<Identity, IEnumerable<Datapoint>> datapoints, Stream stream)
+        {
+            foreach (var kvp in datapoints)
+            {
+                var id = kvp.Key;
+                var dps = kvp.Value;
+                if (!dps.Any()) continue;
+                byte[] idBytes;
+
+                if (id.Id != null)
+                {
+                    idBytes = new byte[sizeof(ushort) + sizeof(long)];
+                    idBytes[0] = idBytes[1] = 0;
+                    Buffer.BlockCopy(BitConverter.GetBytes(id.Id.Value), 0, idBytes, sizeof(ushort), sizeof(long));
+                }
+                else
+                {
+                    idBytes = StringToStorable(id.ExternalId);
+                }
+                stream.Write(idBytes, 0, idBytes.Length);
+
+                var count = dps.Count();
+                stream.Write(BitConverter.GetBytes(count), 0, sizeof(int));
+
+                foreach (var dp in dps)
+                {
+                    var bytes = dp.ToStorableBytes();
+                    stream.Write(bytes, 0, bytes.Length);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Transforms binary encoded datapoints in a stream into a dictionary of datapoints.
+        /// Encoding for one timeseries, ids may repeat:
+        /// [ushort id-length, 0 if internalId]{Either [long internalId] or [ushort size][string]}[uint count][dp 1]...[dp count]
+        /// </summary>
+        /// <param name="stream">Stream to read from</param>
+        /// <param name="token"></param>
+        /// <returns>Read datapoints grouped by identity</returns>
+        public static Task<IDictionary<Identity, IEnumerable<Datapoint>>> ReadDatapointsAsync(Stream stream, CancellationToken token)
+        {
+            return Task.Run(() => ReadDatapoints(stream), token);
+        }
+
+        /// <summary>
+        /// Transforms binary encoded datapoints in a stream into a dictionary of datapoints.
+        /// Encoding for one timeseries, ids may repeat:
+        /// [ushort id-length, 0 if internalId]{Either [long internalId] or [ushort size][string]}[uint count][dp 1]...[dp count]
+        /// </summary>
+        /// <param name="stream">Stream to read from</param>
+        /// <returns>Read datapoints grouped by identity</returns>
+        public static IDictionary<Identity, IEnumerable<Datapoint>> ReadDatapoints(Stream stream)
+        {
+            var ret = new Dictionary<Identity, List<Datapoint>>(new IdentityComparer());
+
+            var idSizeBuffer = new byte[sizeof(ushort)];
+
+            while (true)
+            {
+                if (stream.Read(idSizeBuffer, 0, sizeof(ushort)) < sizeof(ushort)) break;
+                ushort size = BitConverter.ToUInt16(idSizeBuffer, 0);
+
+                Identity id;
+                // A 0-length ID implies that the datapoint uses internalId
+                if (size == 0)
+                {
+                    var idBuffer = new byte[sizeof(long)];
+                    if (stream.Read(idBuffer, 0, sizeof(long)) < sizeof(long)) break;
+                    id = Identity.Create(BitConverter.ToInt64(idBuffer, 0));
+                }
+                else
+                {
+                    string extId = StringFromStream(stream, size);
+                    if (extId == null) break;
+                    id = Identity.Create(extId);
+                }
+
+                var countBuffer = new byte[sizeof(uint)];
+                if (stream.Read(countBuffer, 0, sizeof(uint)) < sizeof(uint)) break;
+                uint count = BitConverter.ToUInt32(countBuffer, 0);
+                if (count == 0) continue;
+
+                var dps = new List<Datapoint>();
+
+                for (int i = 0; i < count; i++)
+                {
+                    var dp = Datapoint.FromStream(stream);
+                    if (dp == null) break;
+
+                    dps.Add(dp);
+                }
+                
+                if (!ret.TryGetValue(id, out var datapoints))
+                {
+                    ret[id] = dps;
+                }
+                else
+                {
+                    datapoints.AddRange(dps);
+                }
+            }
+
+            return ret.ToDictionary(kvp => kvp.Key, kvp => (IEnumerable<Datapoint>)kvp.Value, new IdentityComparer());
+        }
+
+        /// <summary>
+        /// Transforms a single event into an array of bytes.
+        /// Encoding:
+        /// [Id][Start][End][Description][Type][SubType][Source][assetIdCount][dataSetId]{[assetId1]...}[MetaCount]{[key][value]...}
+        /// </summary>
+        /// <param name="evt">Event to write</param>
+        /// <returns>Event serialized as bytes</returns>
+        public static byte[] EventToStorable(EventCreate evt)
+        {
+            var bytes = new List<byte>();
+            bytes.AddRange(StringToStorable(evt.ExternalId));
+            bytes.AddRange(BitConverter.GetBytes(evt.StartTime ?? -1));
+            bytes.AddRange(BitConverter.GetBytes(evt.EndTime ?? -1));
+            bytes.AddRange(StringToStorable(evt.Description));
+            bytes.AddRange(StringToStorable(evt.Type));
+            bytes.AddRange(StringToStorable(evt.Subtype));
+            bytes.AddRange(StringToStorable(evt.Source));
+            bytes.AddRange(BitConverter.GetBytes(evt.DataSetId ?? 0));
+
+            if (evt.AssetIds != null)
+            {
+                var assetIdBytes = new List<byte>();
+                foreach (var id in evt.AssetIds)
+                {
+                    assetIdBytes.AddRange(BitConverter.GetBytes(id));
+                }
+                bytes.AddRange(BitConverter.GetBytes((ushort)evt.AssetIds.Count()));
+                bytes.AddRange(assetIdBytes);
+            }
+            else
+            {
+                bytes.AddRange(new byte[] { 0, 0 });
+            }
+
+
+            if (evt.Metadata != null)
+            {
+                var metaDataBytes = new List<byte>();
+                foreach (var kvp in evt.Metadata)
+                {
+                    metaDataBytes.AddRange(StringToStorable(kvp.Key));
+                    metaDataBytes.AddRange(StringToStorable(kvp.Value));
+                }
+                bytes.AddRange(BitConverter.GetBytes((ushort)evt.Metadata.Count));
+                bytes.AddRange(metaDataBytes);
+            }
+            else
+            {
+                bytes.AddRange(new byte[] { 0, 0 });
+            }
+
+
+            return bytes.ToArray();
+        }
+        /// <summary>
+        /// Reads a single event from a stream.
+        /// Encoding:
+        /// [Id][Start][End][Description][Type][SubType][Source][assetIdCount][dataSetId]{[assetId1]...}[MetaCount]{[key][value]...}
+        /// </summary>
+        /// <param name="stream">Stream to read from</param>
+        /// <returns></returns>
+        public static EventCreate EventFromStream(Stream stream)
+        {
+            var evt = new EventCreate();
+            evt.ExternalId = StringFromStream(stream);
+
+            var timeBytes = new byte[sizeof(long) * 2];
+            if (stream.Read(timeBytes, 0, sizeof(long) * 2) < sizeof(long) * 2) return null;
+            var startTime = BitConverter.ToInt64(timeBytes, 0);
+            var endTime = BitConverter.ToInt64(timeBytes, sizeof(long));
+            if (startTime >= 0) evt.StartTime = startTime;
+            if (endTime >= 0) evt.EndTime = endTime;
+
+            evt.Description = StringFromStream(stream);
+            evt.Type = StringFromStream(stream);
+            evt.Subtype = StringFromStream(stream);
+            evt.Source = StringFromStream(stream);
+
+            var dataSetBytes = new byte[sizeof(long)];
+            if (stream.Read(dataSetBytes, 0, sizeof(long)) < sizeof(long)) return null;
+            long dataSetId = BitConverter.ToInt64(dataSetBytes, 0);
+            if (dataSetId > 0) evt.DataSetId = dataSetId;
+
+            var countBytes = new byte[sizeof(ushort)];
+            if (stream.Read(countBytes, 0, sizeof(ushort)) < sizeof(ushort)) return null;
+            ushort assetIdCount = BitConverter.ToUInt16(countBytes, 0);
+
+            if (assetIdCount > 0)
+            {
+                var assetIds = new List<long>();
+                var idBytes = new byte[sizeof(long)];
+                for (int i = 0; i < assetIdCount; i++)
+                {
+                    if (stream.Read(idBytes, 0, sizeof(long)) < sizeof(long)) return null;
+                    long id = BitConverter.ToInt64(idBytes, 0);
+                    assetIds.Add(id);
+                }
+                evt.AssetIds = assetIds;
+            }
+
+
+            if (stream.Read(countBytes, 0, sizeof(ushort)) < sizeof(ushort)) return null;
+            ushort metaDataCount = BitConverter.ToUInt16(countBytes, 0);
+
+            if (metaDataCount > 0)
+            {
+                evt.Metadata = new Dictionary<string, string>();
+                for (int i = 0; i < metaDataCount; i++)
+                {
+                    var key = StringFromStream(stream);
+                    var value = StringFromStream(stream);
+                    if (key == null) return null;
+                    evt.Metadata[key] = value;
+                }
+            }
+
+
+            return evt;
+        }
+
+        /// <summary>
+        /// Read events from a stream.
+        /// Encoding:
+        /// [Id][Start][End][Description][Type][SubType][Source][assetIdCount][dataSetId]{[assetId1]...}[MetaCount]{[key][value]...}
+        /// </summary>
+        /// <param name="stream">Stream to read from</param>
+        /// <returns>A list of created events</returns>
+        public static IEnumerable<EventCreate> ReadEvents(Stream stream)
+        {
+            var events = new List<EventCreate>();
+            while (true)
+            {
+                var evt = EventFromStream(stream);
+                if (evt == null) break;
+                events.Add(evt);
+            }
+            return events;
+        }
+
+        /// <summary>
+        /// Read events from a stream.
+        /// Encoding:
+        /// [Id][Start][End][Description][Type][SubType][Source][assetIdCount][dataSetId]{[assetId1]...}[MetaCount]{[key][value]...}
+        /// </summary>
+        /// <param name="stream">Stream to read from</param>
+        /// <param name="token"></param>
+        /// <returns>A list of created events</returns>
+        public static Task<IEnumerable<EventCreate>> ReadEventsAsync(Stream stream, CancellationToken token)
+        {
+            return Task.Run(() => ReadEvents(stream), token);
+        }
+
+        /// <summary>
+        /// Write events to a stream.
+        /// Encoding:
+        /// [Id][Start][End][Description][Type][SubType][Source][assetIdCount][dataSetId]{[assetId1]...}[MetaCount]{[key][value]...}
+        /// </summary>
+        /// <param name="events">Events to write</param>
+        /// <param name="stream">Stream to write to</param>
+        public static void WriteEvents(IEnumerable<EventCreate> events, Stream stream)
+        {
+            foreach (var evt in events)
+            {
+                var bytes = EventToStorable(evt);
+                stream.Write(bytes, 0, bytes.Length);
+            }
+        }
+
+        /// <summary>
+        /// Write events to a stream.
+        /// Encoding:
+        /// [Id][Start][End][Description][Type][SubType][Source][assetIdCount][dataSetId]{[assetId1]...}[MetaCount]{[key][value]...}
+        /// </summary>
+        /// <param name="events">Events to write</param>
+        /// <param name="stream">Stream to write to</param>
+        /// <param name="token"></param>
+        public static Task WriteEventsAsync(IEnumerable<EventCreate> events, Stream stream, CancellationToken token)
+        {
+            return Task.Run(() => WriteEvents(events, stream), token);
         }
     }
 
@@ -381,6 +731,11 @@ namespace Cognite.Extractor.Utils
         public double? NumericValue => _numericValue;
 
         /// <summary>
+        /// True if datapoint is string
+        /// </summary>
+        public bool IsString => _numericValue == null;
+
+        /// <summary>
         /// Creates a numeric data point
         /// </summary>
         /// <param name="timestamp">Timestamp</param>
@@ -402,6 +757,87 @@ namespace Cognite.Extractor.Utils
             _timestamp = timestamp.ToUnixTimeMilliseconds();
             _numericValue = null;
             _stringValue = stringValue;
+        }
+        /// <summary>
+        /// Creates a numeric data point
+        /// </summary>
+        /// <param name="timestamp">Timestamp</param>
+        /// <param name="numericValue">double value</param>
+        public Datapoint(long timestamp, double numericValue)
+        {
+            _timestamp = timestamp;
+            _numericValue = numericValue;
+            _stringValue = null;
+        }
+
+        /// <summary>
+        /// Creates a string data point
+        /// </summary>
+        /// <param name="timestamp">Timestamp</param>
+        /// <param name="stringValue">string value</param>
+        public Datapoint(long timestamp, string stringValue)
+        {
+            _timestamp = timestamp;
+            _numericValue = null;
+            _stringValue = stringValue;
+        }
+        /// <summary>
+        /// Convert datapoint into an array of bytes on the form
+        /// [long timestamp][boolean isString]{Either [ushort length][string value] or [double value]}
+        /// </summary>
+        /// <returns></returns>
+        public byte[] ToStorableBytes()
+        {
+            ushort size = sizeof(long) + sizeof(bool);
+
+            byte[] valBytes;
+
+            if (IsString)
+            {
+                valBytes = CogniteUtils.StringToStorable(_stringValue);
+            }
+            else
+            {
+                valBytes = BitConverter.GetBytes(_numericValue.Value);
+            }
+            size += (ushort)valBytes.Length;
+
+            var bytes = new byte[size];
+            int pos = 0;
+            Buffer.BlockCopy(BitConverter.GetBytes(_timestamp), 0, bytes, pos, sizeof(long));
+            pos += sizeof(long);
+            Buffer.BlockCopy(BitConverter.GetBytes(IsString), 0, bytes, pos, sizeof(bool));
+            pos += sizeof(bool);
+
+            Buffer.BlockCopy(valBytes, 0, bytes, pos, valBytes.Length);
+
+            return bytes;
+        }
+        /// <summary>
+        /// Initializes Datapoint by reading from a stream. Requires that the next bytes in the stream represent a datapoint.
+        /// </summary>
+        /// <param name="stream">Stream to read from</param>
+        public static Datapoint FromStream(Stream stream)
+        {
+            var baseBytes = new byte[sizeof(long) + sizeof(bool)];
+            int read = stream.Read(baseBytes, 0, sizeof(long) + sizeof(bool));
+            if (read < sizeof(long) + sizeof(bool)) return null;
+
+            var timestamp = BitConverter.ToInt64(baseBytes, 0);
+            var isString = BitConverter.ToBoolean(baseBytes, sizeof(long));
+
+            if (isString)
+            {
+                string value = CogniteUtils.StringFromStream(stream);
+                return new Datapoint(timestamp, value);
+            }
+            else
+            {
+                var valueBytes = new byte[sizeof(double)];
+                if (stream.Read(valueBytes, 0, sizeof(double)) < sizeof(double)) return null;
+                double value = BitConverter.ToDouble(valueBytes, 0);
+                return new Datapoint(timestamp, value);
+            }
         }
     }
 }
