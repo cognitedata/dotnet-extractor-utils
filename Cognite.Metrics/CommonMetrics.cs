d5f7f668fed6a56ca24279bae7d1ed234d65eb62
Einar Omang <einar@omang.com>
2020-07-24 13:59:30 +0200

    Common metrics
    Includes a system to embed version numbers in the assembly, and some
    common metrics.

---
+++ Cognite.Metrics/CommonMetrics.cs
@@ -0,0 +1,41 @@
+﻿using Prometheus;
+
+namespace Cognite.Extractor.Metrics
+{
+    /// <summary>
+    /// Class containing common metrics
+    /// </summary>
+    public static class CommonMetrics
+    {
+        private static Gauge _version = Prometheus.Metrics.CreateGauge("extractor_utils_info",
+            "Information about the running extractor", "name", "version");
+        private static Gauge _startTime = Prometheus.Metrics.CreateGauge("extractor_utils_start_time", "Time the extractor last started");
+        private static Gauge _endTime = Prometheus.Metrics.CreateGauge("extractor_utils_stop_time", "Time the extractor last stopped");
+
+        /// <summary>
+        /// Set metrics with information about extractor name and version
+        /// </summary>
+        /// <param name="name">Name of the extractor</param>
+        /// <param name="version">Version of the extractor</param>
+        public static void SetInfo(string name, string version)
+        {
+            _version.WithLabels(name, version).Set(0);
+        }
+
+        /// <summary>
+        /// Set start time gauge to now in utc.
+        /// </summary>
+        public static void SetStartTime()
+        {
+            _startTime.SetToCurrentTimeUtc();
+        }
+
+        /// <summary>
+        /// Set end time gauge to now in utc.
+        /// </summary>
+        public static void SetEndTime()
+        {
+            _endTime.SetToCurrentTimeUtc();
+        }
+    }
+}
---
+++ Cognite.Metrics/Version.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace Cognite.Extractor.Metrics
+{
+    /// <summary>
+    /// Utility class for reading version information from properties.
+    /// In order to use this, version information has to be added to the assembly.
+    /// See the online documentation for an example.
+    /// </summary>
+    public static class Version
+    {
+        /// <summary>
+        /// Read a property from the calling assembly.
+        /// </summary>
+        /// <param name="property">Property to read</param>
+        /// <param name="assembly">Assembly to use, default is the assembly that calls this method.</param>
+        /// <returns>null or the value of the property in the assembly</returns>
+        public static string Read(string property, Assembly assembly = null)
+        {
+            if (assembly == null)
+            {
+                assembly = Assembly.GetCallingAssembly();
+            }
+            using (var stream = assembly.GetManifestResourceStream($"{assembly.GetName().Name}.Properties.{property}"))
+            {
+                if (stream == null) return null;
+                using (var reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd().Trim();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get status on the form [GitCommitHash] [GitCommitTime].
+        /// </summary>
+        /// <returns></returns>
+        public static string Status()
+        {
+            string hash = Read("GitCommitHash", Assembly.GetCallingAssembly());
+            string time = Read("GitCommitTime", Assembly.GetCallingAssembly());
+            return $"{hash} {time}";
+        }
+        /// <summary>
+        /// Get the version, just the GitCommitHash. This can be parsed further to produce a readable string.
+        /// </summary>
+        /// <returns></returns>
+        public static string GetVersion()
+        {
+            return Read("GitCommitHash", Assembly.GetCallingAssembly());
+        }
+    }
+}
