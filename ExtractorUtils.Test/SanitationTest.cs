191ab3ca346590a63f21a16743a3876f4e3faaaf
Einar Omang <einar@omang.com>
2020-08-31 12:42:37 +0200

    Add some tests

--- ExtractorUtils.Test/SanitationTest.cs
+++ ExtractorUtils.Test/SanitationTest.cs
@@ -45,6 +45,47 @@ namespace ExtractorUtils.Test
             Assert.Equal(new string('æ', 128), asset.Source);
         }
         [Fact]
+        public void TestVerifyAsset()
+        {
+            var removeFields = new List<ResourceType>
+            {
+                ResourceType.ExternalId, ResourceType.Name, ResourceType.ParentId,
+                ResourceType.ParentExternalId, ResourceType.Description, ResourceType.DataSetId,
+                ResourceType.Metadata, ResourceType.Source, ResourceType.Labels
+            };
+            var asset = new AssetCreate
+            {
+                ExternalId = new string('æ', 300),
+                Description = new string('æ', 1000),
+                DataSetId = -2502,
+                Labels = new CogniteExternalId[] { null, new CogniteExternalId(null) }.Concat(Enumerable.Range(0, 100).Select(i => new CogniteExternalId(new string('æ', 300)))),
+                Metadata = Enumerable.Range(0, 100)
+                    .ToDictionary(i => $"key{i.ToString("000")}{new string('æ', 100)}", i => new string('æ', 200)),
+                Name = new string('ø', 1000),
+                ParentExternalId = new string('æ', 300),
+                ParentId = -1234,
+                Source = new string('æ', 12345)
+            };
+            foreach (var field in removeFields)
+            {
+                var errType = asset.Verify();
+                Assert.Equal(field, errType);
+                switch (field)
+                {
+                    case ResourceType.ExternalId: asset.ExternalId = null; break;
+                    case ResourceType.Name: asset.Name = null; break;
+                    case ResourceType.ParentId: asset.ParentId = null; break;
+                    case ResourceType.ParentExternalId: asset.ParentExternalId = null; break;
+                    case ResourceType.Description: asset.Description = null; break;
+                    case ResourceType.DataSetId: asset.DataSetId = null; break;
+                    case ResourceType.Metadata: asset.Metadata = null; break;
+                    case ResourceType.Source: asset.Source = null; break;
+                    case ResourceType.Labels: asset.Labels = null; break;
+                }
+            }
+            Assert.Null(asset.Verify());
+        }
+        [Fact]
         public void TestSanitizeTimeSeries()
         {
             var ts = new TimeSeriesCreate
@@ -73,6 +114,45 @@ namespace ExtractorUtils.Test
             Assert.All(ts.Metadata, kvp => Assert.Equal(new string('æ', 200), kvp.Value));
             Assert.Equal(new string('æ', 255), ts.Name);
             Assert.Equal(new string('æ', 32), ts.Unit);
+        }
+        [Fact]
+        public void TestVerifyTimeSeries()
+        {
+            var removeFields = new List<ResourceType>
+            {
+                ResourceType.ExternalId, ResourceType.Name, ResourceType.AssetId,
+                ResourceType.Description, ResourceType.DataSetId, ResourceType.Metadata,
+                ResourceType.Unit, ResourceType.LegacyName
+            };
+            var ts = new TimeSeriesCreate
+            {
+                ExternalId = new string('æ', 300),
+                Description = new string('æ', 2000),
+                DataSetId = -2952,
+                AssetId = -1239,
+                LegacyName = new string('æ', 300),
+                Metadata = Enumerable.Range(0, 100)
+                    .ToDictionary(i => $"key{i.ToString("000")}{new string('æ', 100)}", i => new string('æ', 200)),
+                Name = new string('æ', 300),
+                Unit = new string('æ', 200)
+            };
+            foreach (var field in removeFields)
+            {
+                var errType = ts.Verify();
+                Assert.Equal(field, errType);
+                switch (field)
+                {
+                    case ResourceType.ExternalId: ts.ExternalId = null; break;
+                    case ResourceType.Name: ts.Name = null; break;
+                    case ResourceType.AssetId: ts.AssetId = null; break;
+                    case ResourceType.Description: ts.Description = null; break;
+                    case ResourceType.DataSetId: ts.DataSetId = null; break;
+                    case ResourceType.Metadata: ts.Metadata = null; break;
+                    case ResourceType.Unit: ts.Unit = null; break;
+                    case ResourceType.LegacyName: ts.LegacyName = null; break;
+                }
+            }
+            Assert.Null(ts.Verify());
         }
         [Fact]
         public void TestSanitizeEvent()
@@ -105,6 +185,50 @@ namespace ExtractorUtils.Test
             Assert.Equal(new string('æ', 128), evt.Source);
             Assert.Equal(new string('æ', 64), evt.Type);
             Assert.Equal(new string('æ', 64), evt.Subtype);
+        }
+        [Fact]
+        public void TestVerifyEvent()
+        {
+            var removeFields = new List<ResourceType>
+            {
+                ResourceType.ExternalId, ResourceType.Type, ResourceType.SubType,
+                ResourceType.Source, ResourceType.AssetId, ResourceType.TimeRange,
+                ResourceType.DataSetId, ResourceType.Metadata
+            };
+            var evt = new EventCreate
+            {
+                AssetIds = Enumerable.Range(-100, 100000).Select(i => (long)i),
+                DataSetId = -125,
+                Description = new string('æ', 1000),
+                EndTime = -12345,
+                StartTime = -12345,
+                ExternalId = new string('æ', 300),
+                Metadata = Enumerable.Range(0, 200)
+                    .ToDictionary(i => $"key{i.ToString("000")}{new string('æ', 100)}", i => new string('æ', 600)),
+                Source = new string('æ', 200),
+                Subtype = new string('æ', 300),
+                Type = new string('æ', 300)
+            };
+            foreach (var field in removeFields)
+            {
+                var errType = evt.Verify();
+                Assert.Equal(field, errType);
+                switch (field)
+                {
+                    case ResourceType.ExternalId: evt.ExternalId = null; break;
+                    case ResourceType.Type: evt.Type = null; break;
+                    case ResourceType.SubType: evt.Subtype = null; break;
+                    case ResourceType.Source: evt.Source = null; break;
+                    case ResourceType.AssetId: evt.AssetIds = null; break;
+                    case ResourceType.TimeRange:
+                        evt.StartTime = 100;
+                        evt.EndTime = 1000;
+                        break;
+                    case ResourceType.DataSetId: evt.DataSetId = null; break;
+                    case ResourceType.Metadata: evt.Metadata = null; break;
+                }
+            }
+            Assert.Null(evt.Verify());
         }
         [Theory]
         [InlineData("æææææ", 4)]
