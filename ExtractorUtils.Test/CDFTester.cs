e0a14195c0b2d97b8ffb626f5a5747348a7f0472
Einar Marstrander Omang <einar.omang@cognite.com>
2020-10-19 12:16:01 +0200

    Integration tests (#71)
    Add framework for integration tests, and add a few for assets, timeseries and events.
    
    The most important features to test are handling of CDF errors, since this behavior may change unexpectedly. The old mock-tests are kept for now, since it is difficult to replicate some behavior, and it doesn't hurt to keep them.

---
+++ ExtractorUtils.Test/CDFTester.cs
@@ -0,0 +1,61 @@
+﻿using Cognite.Extractor.Logging;
+using Cognite.Extractor.Utils;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Threading;
+
+namespace ExtractorUtils.Test
+{
+    class CDFTester : IDisposable
+    {
+        private static int _configIdx;
+        public ServiceProvider Provider { get; }
+        public CogniteDestination Destination { get; }
+        public CancellationTokenSource Source { get; }
+        public string Project { get; private set; }
+        public string Host { get; private set; }
+        public string Prefix { get; private set; }
+        private readonly string _configPath;
+
+        public CDFTester(string[] configLines)
+        {
+            // Thread safe increment and store
+            _configPath = $"test-config-{Interlocked.Increment(ref _configIdx)}";
+            System.IO.File.WriteAllLines(_configPath, configLines);
+            var services = new ServiceCollection();
+            services.AddConfig<BaseConfig>(_configPath, 2);
+            services.AddLogger();
+            services.AddCogniteClient("net-extractor-utils-test");
+            Provider = services.BuildServiceProvider();
+            Destination = Provider.GetRequiredService<CogniteDestination>();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            Random random = new Random();
+            Prefix = "net-utils-test-" + new string(Enumerable.Repeat(chars, 5)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+            Source = new CancellationTokenSource();
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    System.IO.File.Delete(_configPath);
+                    Provider.Dispose();
+                    Source.Dispose();
+                }
+                disposedValue = true;
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+    }
+}
