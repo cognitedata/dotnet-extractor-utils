70879d4e65c55afc68d661082fe4e5df46ce5082
Lucas Provensi <lucas.provensi@cognite.com>
2021-03-31 12:40:29 +0200

    Add integration test for data points upload queue

--- ExtractorUtils.Test/TimeSeriesIntegrationTest.cs
+++ ExtractorUtils.Test/TimeSeriesIntegrationTest.cs
@@ -1,5 +1,7 @@
 ﻿using Cognite.Extensions;
+using Cognite.Extractor.Common;
 using CogniteSdk;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -247,6 +249,105 @@ namespace ExtractorUtils.Test
                     $"{tester.Prefix} missing-dataset-id",
                     $"{tester.Prefix} missing-dataset-id-2",
                     $"{tester.Prefix} final-ts-ok"
+                };
+                await tester.Destination.CogniteClient.TimeSeries.DeleteAsync(new TimeSeriesDelete
+                {
+                    IgnoreUnknownIds = true,
+                    Items = ids.Select(Identity.Create)
+                });
+            }
+        }
+
+        [Theory]
+        [InlineData(CogniteHost.GreenField)]
+        [InlineData(CogniteHost.BlueField)]
+        public async Task TestUploadQueue(CogniteHost host)
+        {
+            using var tester = new CDFTester(host);
+
+            var timeseries = new[]
+            {
+                new TimeSeriesCreate
+                {
+                    ExternalId = $"{tester.Prefix} dp-ts-1"
+                },
+                new TimeSeriesCreate
+                {
+                    ExternalId = $"{tester.Prefix} dp-ts-2"
+                },
+                new TimeSeriesCreate
+                {
+                    ExternalId = $"{tester.Prefix} dp-ts-3",
+                }
+            };
+            int dpCount = 0;
+            int cbCount = 0;
+            var startTime = DateTime.UtcNow;
+            try
+            {
+                var result = await tester.Destination.EnsureTimeSeriesExistsAsync(timeseries, RetryMode.OnError, SanitationMode.Remove, tester.Source.Token);
+                tester.Logger.LogResult(result, RequestType.CreateTimeSeries, false);
+
+                using (var queue = tester.Destination.CreateTimeSeriesUploadQueue(TimeSpan.FromSeconds(1), 0, res =>
+                {
+                    dpCount += res.Uploaded?.Count() ?? 0;
+                    cbCount++;
+                    tester.Logger.LogInformation("Sent {Num} data points to CDF", dpCount);
+                    return Task.CompletedTask;
+                }))
+                {
+                    var enqueueTask = Task.Run(async () => {
+                        for (int i =0; i < 20; ++i)
+                        {
+                            queue.Enqueue(timeseries[0].ExternalId, new Datapoint(DateTime.UtcNow, i));
+                            queue.Enqueue(timeseries[1].ExternalId, new Datapoint(DateTime.UtcNow, i));
+                            queue.Enqueue(timeseries[2].ExternalId, new Datapoint(DateTime.UtcNow, i));
+                            await Task.Delay(100, tester.Source.Token);
+                        }
+                    });
+                    var uploadTask = queue.Start(tester.Source.Token);
+
+                    var t = Task.WhenAny(uploadTask, enqueueTask);
+                    await t;
+                    tester.Logger.LogInformation("Enqueueing task completed. Disposing of the upload queue");
+                }
+                Assert.Equal(3 * 20, dpCount);
+                Assert.True(cbCount > 1);
+
+                var query = new List<DataPointsQueryItem>();
+                foreach (var ts in timeseries) 
+                {
+                    var queryItem = new DataPointsQueryItem()
+                    {
+                        ExternalId = ts.ExternalId,
+                        Start = $"{startTime.ToUnixTimeMilliseconds()}",
+                        End = $"{DateTime.UtcNow.ToUnixTimeMilliseconds() + 1}",
+                        Limit = 100
+                    };
+                    query.Add(queryItem);
+                }
+                var dataPointsQuery = new DataPointsQuery()
+                {
+                    Items = query
+                };
+
+                var dpsFromCDF = await tester.Destination.CogniteClient.DataPoints.ListAsync(dataPointsQuery, tester.Source.Token);
+                Assert.Equal(3, dpsFromCDF.Items.Count);
+                foreach (var item in dpsFromCDF.Items)
+                {
+                    Assert.NotNull(item.NumericDatapoints);
+                    Assert.Null(item.StringDatapoints);
+                    Assert.Equal(20, item.NumericDatapoints.Datapoints.Count);
+                }
+
+            }
+            finally
+            {
+                var ids = new[]
+                {
+                    timeseries[0].ExternalId,
+                    timeseries[1].ExternalId,
+                    timeseries[2].ExternalId
                 };
                 await tester.Destination.CogniteClient.TimeSeries.DeleteAsync(new TimeSeriesDelete
                 {
