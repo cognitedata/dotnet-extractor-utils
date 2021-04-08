1381fc529bd38ddb22624f7cc82cbe7eba290514
Lucas Provensi <lucas.provensi@cognite.com>
2021-04-08 21:50:22 +0200

    Test upload queue

--- ExtractorUtils.Test/RawIntegrationTest.cs
+++ ExtractorUtils.Test/RawIntegrationTest.cs
@@ -1,5 +1,6 @@
 using Cognite.Extensions;
 using CogniteSdk;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,7 +21,7 @@ namespace ExtractorUtils.Test
 
 
         [Theory]
-        //[InlineData(CogniteHost.GreenField)]
+        [InlineData(CogniteHost.GreenField)]
         [InlineData(CogniteHost.BlueField)]
         public async Task TestInsertRow(CogniteHost host)
         {
@@ -52,11 +53,69 @@ namespace ExtractorUtils.Test
                     Assert.Equal(number, columns[i.Key].Number);
                 });
 
-                await tester.Destination.DeleteRowsAsync(dbName, tableName, columns.Keys, tester.Source.Token);
-                rows = await tester.Destination.CogniteClient.Raw.ListRowsAsync(dbName, tableName, tester.Source.Token);
-                Assert.Empty(rows.Items);
+                if (host != CogniteHost.BlueField)
+                {
+                    // TODO: deleting rows is not working in Bluefield. Enable this once it works.
+                    await tester.Destination.DeleteRowsAsync(dbName, tableName, columns.Keys, tester.Source.Token);
+                    rows = await tester.Destination.CogniteClient.Raw.ListRowsAsync(dbName, tableName, tester.Source.Token);
+                    Assert.Empty(rows.Items);
+                }
             }
             finally
+            {
+                await tester.Destination.CogniteClient.Raw.DeleteDatabasesAsync(new List<string>(){ dbName }, true, tester.Source.Token);
+            }
+        }
+
+        [Theory]
+        [InlineData(CogniteHost.GreenField)]
+        [InlineData(CogniteHost.BlueField)]
+        public async Task TestUploadQueue(CogniteHost host)
+        {
+            using var tester = new CDFTester(host);
+
+            string dbName = $"{tester.Prefix}-Db";
+            string tableName = $"{tester.Prefix}-Table";
+
+            var totalUploaded = 0;
+            try {
+                using (var queue = tester.Destination.CreateRawUploadQueue<TestDto>(dbName, tableName, TimeSpan.FromSeconds(1), 0, res => {
+                    var numUploaded = res.Uploaded?.Count() ?? 0;
+                    totalUploaded += numUploaded;
+                    tester.Logger.LogInformation("Sent {Num} raw rows to CDF", numUploaded);
+                    return Task.CompletedTask;
+                }))
+                {
+                   var enqueueTask = Task.Run(async () => {
+                        for (int i = 0; i < 20; ++i)
+                        {
+                            queue.EnqueueRow($"r{i}", new TestDto {Name = $"Test {i}", Number = i});
+                            await Task.Delay(100, tester.Source.Token);
+                        }
+                    });
+                    var uploadTask = queue.Start(tester.Source.Token);
+
+                    var t = Task.WhenAny(uploadTask, enqueueTask);
+                    await t;
+
+                    tester.Logger.LogInformation("Enqueueing task completed. Disposing of the upload queue");
+                }
+                Assert.Equal(20, totalUploaded);
+
+                var rows = await tester.Destination.CogniteClient.Raw.ListRowsAsync(dbName, tableName, tester.Source.Token);
+                Assert.Equal(20, rows.Items.Count());
+
+                var indexes = Enumerable.Range(0, 20).Select(i => $"r{i}").ToList();
+                Assert.All(rows.Items, (i) =>{
+
+                    var name = i.Columns["Name"].ToString();
+                    Assert.True(i.Columns["Number"].TryGetInt32(out var number));
+                    Assert.Contains(i.Key, indexes);
+                    Assert.Equal(name, $"Test {number}");
+                });
+
+            }
+            finally 
             {
                 await tester.Destination.CogniteClient.Raw.DeleteDatabasesAsync(new List<string>(){ dbName }, true, tester.Source.Token);
             }
