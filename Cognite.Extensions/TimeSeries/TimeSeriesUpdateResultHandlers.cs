3a961604ed25ffacf4d71aa7da9a133a66f6b76f
Einar Omang <einar@omang.com>
2021-12-06 14:58:45 +0100

    Add sanitation and result handlers

---
+++ Cognite.Extensions/TimeSeries/TimeSeriesUpdateResultHandlers.cs
@@ -0,0 +1,138 @@
+﻿using CogniteSdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cognite.Extensions
+{
+    public partial class ResultHandlers
+    {
+        private static void ParseTimeSeriesUpdateException(ResponseException ex, CogniteError err)
+        {
+            if (ex.Missing?.Any() ?? false)
+            {
+                if (ex.Message.StartsWith("Time series ids not found", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    err.Type = ErrorType.ItemMissing;
+                    err.Resource = ResourceType.Id;
+                    err.Values = ex.Missing.Select(dict =>
+                    {
+                        if (dict.TryGetValue("id", out var idVal) && idVal is MultiValue.Long longIdVal)
+                        {
+                            return Identity.Create(longIdVal.Value);
+                        }
+                        else if (dict.TryGetValue("externalId", out var extIdVal)
+                            && extIdVal is MultiValue.String strIdVal
+                            && strIdVal.Value != null)
+                        {
+                            return Identity.Create(strIdVal.Value);
+                        }
+                        return null!;
+                    }).Where(id => id != null);
+                }
+                else if (ex.Message.StartsWith("Asset ids not found", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    err.Type = ErrorType.ItemMissing;
+                    err.Resource = ResourceType.AssetId;
+                    err.Values = ex.Missing.Select(dict
+                        => (dict["id"] as MultiValue.Long)?.Value)
+                        .Where(id => id.HasValue)
+                        .Select(id => Identity.Create(id!.Value));
+                }
+                else if (ex.Message.StartsWith("Datasets ids not found", StringComparison.InvariantCultureIgnoreCase)
+                        || ex.Message.StartsWith("Data set ids not found", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    err.Type = ErrorType.ItemMissing;
+                    err.Resource = ResourceType.DataSetId;
+                    err.Values = ex.Missing.Select(dict
+                        => (dict["id"] as MultiValue.Long)?.Value)
+                        .Where(id => id.HasValue)
+                        .Select(id => Identity.Create(id!.Value));
+                }
+            }
+            else if (ex.Duplicated?.Any() ?? false)
+            {
+                err.Type = ErrorType.ItemExists;
+                err.Resource = ResourceType.ExternalId;
+                err.Values = ex.Duplicated.Select(dict
+                    => (dict["externalId"] as MultiValue.String)?.Value)
+                    .Where(id => id != null)
+                    .Select(Identity.Create);
+            }
+        }
+
+        /// <summary>
+        /// Clean list of TimeSeriesUpdateItem objects based on CogniteError
+        /// </summary>
+        /// <param name="error">Error that occured with a previous push</param>
+        /// <param name="items">Timeseries updates to clean</param>
+        /// <returns>TimeSeries updates that are not affected by the error</returns>
+        public static IEnumerable<TimeSeriesUpdateItem> CleanFromError(
+            CogniteError<TimeSeriesUpdateItem> error,
+            IEnumerable<TimeSeriesUpdateItem> items)
+        {
+            if (items == null)
+            {
+                throw new ArgumentNullException(nameof(items));
+            }
+            if (error == null) return items;
+            if (!error.Values?.Any() ?? true)
+            {
+                error.Values = items.Where(ts => ts.ExternalId != null).Select(ts => Identity.Create(ts.ExternalId));
+                return Array.Empty<TimeSeriesUpdateItem>();
+            }
+
+            var badValues = new HashSet<Identity>(error.Values);
+
+            var ret = new List<TimeSeriesUpdateItem>();
+            var skipped = new List<TimeSeriesUpdateItem>();
+
+            foreach (var item in items)
+            {
+                bool added = false;
+                var update = item.Update;
+
+                switch (error.Resource)
+                {
+                    case ResourceType.DataSetId:
+                        if (update.DataSetId?.Set == null
+                            || !badValues.Contains(Identity.Create(update.DataSetId.Set.Value))) added = true;
+                        break;
+                    case ResourceType.ExternalId:
+                        if (update.ExternalId?.Set == null
+                            || !badValues.Contains(Identity.Create(update.ExternalId.Set))) added = true;
+                        break;
+                    case ResourceType.AssetId:
+                        if (update.AssetId?.Set == null
+                            || !badValues.Contains(Identity.Create(update.AssetId.Set.Value))) added = true;
+                        break;
+                    case ResourceType.Id:
+                        var idt = item.Id.HasValue ? Identity.Create(item.Id.Value) : Identity.Create(item.ExternalId);
+
+                        if (!badValues.Contains(idt)) added = true;
+                        break;
+                }
+                if (added)
+                {
+                    ret.Add(item);
+                }
+                else
+                {
+                    CdfMetrics.TimeSeriesSkipped.Inc();
+                    skipped.Add(item);
+                }
+            }
+            if (skipped.Any())
+            {
+                error.Skipped = skipped;
+            }
+            else
+            {
+                error.Skipped = items;
+                return Enumerable.Empty<TimeSeriesUpdateItem>();
+            }
+            return ret;
+        }
+    }
+}
---
+++ Cognite.Extensions/TimeSeries/TimeSeriesUpdateSanitation.cs
@@ -0,0 +1,147 @@
+﻿using CogniteSdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cognite.Extensions
+{
+    public partial class Sanitation
+    {
+        /// <summary>
+        /// Sanitize a TimeSeriesUpdateItem object so that it can be safely sent to CDF.
+        /// Requests may still fail due to conflicts or missing ids.
+        /// </summary>
+        /// <param name="item">TimeSeries update to sanitize</param>
+        public static void Sanitize(this TimeSeriesUpdateItem item)
+        {
+            if (item == null) throw new ArgumentNullException(nameof(item));
+            if (item.Id == null) item.ExternalId = item.ExternalId.Truncate(ExternalIdMax);
+
+            var update = item.Update;
+            update.ExternalId = update.ExternalId.Truncate(ExternalIdMax);
+            update.Name = update.Name.Truncate(TimeSeriesNameMax);
+            if (update.AssetId?.Set != null && update.AssetId.Set < 1) update.AssetId = null;
+            update.Description = update.Description.Truncate(TimeSeriesDescriptionMax);
+            if (update.DataSetId?.Set != null && update.DataSetId.Set < 1) update.DataSetId = null;
+            if (update.Metadata != null)
+            {
+                update.Metadata.Add = update.Metadata.Add.SanitizeMetadata(
+                    TimeSeriesMetadataMaxPerKey, TimeSeriesMetadataMaxPairs,
+                    TimeSeriesMetadataMaxPerValue, TimeSeriesMetadataMaxBytes, out _);
+                update.Metadata.Set = update.Metadata.Set.SanitizeMetadata(
+                    TimeSeriesMetadataMaxPerKey, TimeSeriesMetadataMaxPairs,
+                    TimeSeriesMetadataMaxPerValue, TimeSeriesMetadataMaxBytes, out _);
+            }
+            update.Unit = update.Unit.Truncate(TimeSeriesUnitMax);
+        }
+
+        /// <summary>
+        /// Check that given TimeSeriesUpdateItem satisfies CDF limits.
+        /// Update may fail if new size of timeseries metadata exceeds limit for total size.
+        /// </summary>
+        /// <param name="item">Timeseries update to check</param>
+        /// <returns>True if timeseries update satisfies limits</returns>
+        public static ResourceType? Verify(this TimeSeriesUpdateItem item)
+        {
+            if (item == null) throw new ArgumentNullException(nameof(item));
+            if (!item.ExternalId.CheckLength(ExternalIdMax)) return ResourceType.ExternalId;
+            if (item.Id != null && item.Id < 1) return ResourceType.Id;
+            if ((item.Id == null) == (item.ExternalId == null)) return ResourceType.Id;
+
+            var update = item.Update;
+            if (!update.ExternalId?.Set?.CheckLength(ExternalIdMax) ?? false) return ResourceType.ExternalId;
+            if (!update.Name?.Set?.CheckLength(TimeSeriesNameMax) ?? false) return ResourceType.Name;
+            if (update.AssetId?.Set != null && update.AssetId.Set < 1) return ResourceType.AssetId;
+            if (!update.Description?.Set?.CheckLength(TimeSeriesDescriptionMax) ?? false) return ResourceType.Description;
+            if (update.DataSetId?.Set != null && update.DataSetId.Set < 1) return ResourceType.DataSetId;
+            if (update.Metadata != null)
+            {
+                if (!update.Metadata.Set.VerifyMetadata(
+                    TimeSeriesMetadataMaxPerKey, TimeSeriesMetadataMaxPairs,
+                    TimeSeriesMetadataMaxPerValue, TimeSeriesMetadataMaxBytes, out _))
+                    return ResourceType.Metadata;
+                if (!update.Metadata.Add.VerifyMetadata(
+                    TimeSeriesMetadataMaxPerKey, TimeSeriesMetadataMaxPairs,
+                    TimeSeriesMetadataMaxPerValue, TimeSeriesMetadataMaxBytes, out _))
+                    return ResourceType.Metadata;
+            }
+            if (!update.Unit?.Set?.CheckLength(TimeSeriesUnitMax) ?? false) return ResourceType.Unit;
+
+            return null;
+        }
+
+        /// <summary>
+        /// Clean list of TimeSeriesUpdateItem objects, sanitizing each and removing any duplicates.
+        /// The first encountered duplicate is kept.
+        /// </summary>
+        /// <param name="items">TimeSeriesUpdate request to clean</param>
+        /// <param name="mode">The type of sanitation to apply</param>
+        /// <returns>Cleaned update request and a list of errors</returns>
+        public static (IEnumerable<TimeSeriesUpdateItem>, IEnumerable<CogniteError<TimeSeriesUpdateItem>>) CleanTimeSeriesUpdateRequest(
+            IEnumerable<TimeSeriesUpdateItem> items,
+            SanitationMode mode)
+        {
+            if (mode == SanitationMode.None) return (items, Enumerable.Empty<CogniteError<TimeSeriesUpdateItem>>());
+            if (items == null) throw new ArgumentNullException(nameof(items));
+
+            var result = new List<TimeSeriesUpdateItem>();
+            var errors = new List<CogniteError<TimeSeriesUpdateItem>>();
+
+            var ids = new HashSet<Identity>();
+            var duplicated = new HashSet<Identity>();
+            var bad = new List<(ResourceType, TimeSeriesUpdateItem)>();
+
+            foreach (var item in items)
+            {
+                bool toAdd = true;
+                if (mode == SanitationMode.Clean)
+                {
+                    item.Sanitize();
+                }
+                var failedField = item.Verify();
+                if (failedField.HasValue)
+                {
+                    bad.Add((failedField.Value, item));
+                    toAdd = false;
+                }
+
+                if (item.ExternalId == null && item.Id == null) continue;
+
+                var idt = item.ExternalId != null ? Identity.Create(item.ExternalId) : Identity.Create(item.Id!.Value);
+                if (!ids.Add(idt))
+                {
+                    duplicated.Add(idt);
+                    toAdd = false;
+                }
+
+                if (toAdd)
+                {
+                    result.Add(item);
+                }
+            }
+            if (duplicated.Any())
+            {
+                errors.Add(new CogniteError<TimeSeriesUpdateItem>
+                {
+                    Status = 409,
+                    Message = "Duplicate ids",
+                    Resource = ResourceType.Id,
+                    Type = ErrorType.ItemDuplicated,
+                    Values = duplicated
+                });
+            }
+            if (bad.Any())
+            {
+                errors.AddRange(bad.GroupBy(pair => pair.Item1).Select(group => new CogniteError<TimeSeriesUpdateItem>
+                {
+                    Skipped = group.Select(pair => pair.Item2).ToList(),
+                    Resource = group.Key,
+                    Type = ErrorType.SanitationFailed,
+                    Status = 400
+                }));
+            }
+            return (result, errors);
+        }
+    }
+}
