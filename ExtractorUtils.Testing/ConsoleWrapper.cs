d494a57727c786f501a91519c20f4f6450ae5ddc
Einar Omang <einar@omang.com>
2022-01-24 12:16:02 +0100

    Add test utilities project
    Contains a few useful classes and methods for testing with the extractor
    utils.
    
     - System to log serilog or console to test output.
     - The WaitForCondition utility.

---
+++ ExtractorUtils.Testing/ConsoleWrapper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using Xunit.Abstractions;
+
+namespace Cognite.ExtractorUtils.Testing
+{
+    /// <summary>
+    /// Base class for test classes that dump console output to the xunit test output.
+    /// Requires disabling test parallelization. A better solution is to use TestUtils.AddTestLogger
+    /// but this requires the code to use dependency injected loggers, and that they are
+    /// instantiated after 
+    /// </summary>
+    public class ConsoleWrapper : IDisposable
+    {
+        private readonly ITestOutputHelper _output;
+        private readonly TextWriter _originalOut;
+        private readonly TextWriter _textWriter;
+        private bool disposed;
+
+        /// <summary>
+        /// Constructor, pass the test output helper from the test class.
+        /// </summary>
+        /// <param name="output"></param>
+        public ConsoleWrapper(ITestOutputHelper output)
+        {
+            _output = output;
+            _originalOut = Console.Out;
+            _textWriter = new StringWriter();
+            Console.SetOut(_textWriter);
+        }
+
+        /// <inheritDoc />
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Internal dispose method, should be called if overridden in subclass.
+        /// </summary>
+        /// <param name="disposing">True if we are disposing disposables now</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed) return;
+            if (disposing)
+            {
+                Console.SetOut(_originalOut);
+                // This can rarely randomly fail due to some obscure threading issue.
+                // It's just cleanup of tests, so we can just retry.
+                try
+                {
+                    _output.WriteLine(_textWriter.ToString());
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    _output.WriteLine(_textWriter.ToString());
+                }
+                _textWriter.Dispose();
+            }
+            disposed = true;
+        }
+    }
+}
---
+++ ExtractorUtils.Testing/TestUtils.cs
@@ -0,0 +1,97 @@
+﻿using Cognite.Extractor.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Cognite.ExtractorUtils.Testing
+{
+    /// <summary>
+    /// Collection of static utility methods for testing
+    /// </summary>
+    public static class TestUtils
+    {
+        /// <summary>
+        /// Returns a task that polls an asynchronous method every 200 milliseconds and returns once it
+        /// is true. This throws an exception if the condition does not become true within <paramref name="seconds"/>.
+        /// </summary>
+        /// <param name="condition">Method to poll</param>
+        /// <param name="seconds">Number of seconds to wait before failing</param>
+        /// <param name="assertion">Method returning a string to include in the thrown exception</param>
+        /// <exception cref="ArgumentNullException">if condition or assertion are null</exception>
+        /// <exception cref="TrueException">
+        /// If <paramref name="condition"/> does not become true within <paramref name="seconds"/>
+        /// </exception>
+        public static async Task WaitForCondition(Func<Task<bool>> condition, int seconds, Func<string> assertion)
+        {
+            if (condition == null) throw new ArgumentNullException(nameof(condition));
+            if (assertion == null) throw new ArgumentNullException(nameof(assertion));
+            bool triggered = false;
+            int i;
+            for (i = 0; i < seconds * 5; i++)
+            {
+                if (await condition().ConfigureAwait(false))
+                {
+                    triggered = true;
+                    break;
+                }
+
+                await Task.Delay(200).ConfigureAwait(false);
+            }
+
+            Assert.True(triggered, assertion());
+        }
+        /// <summary>
+        /// Returns a task that polls a method every 200 milliseconds and returns once it
+        /// is true. This throws an exception if the condition does not become true within <paramref name="seconds"/>.
+        /// </summary>
+        /// <param name="condition">Method to poll</param>
+        /// <param name="seconds">Number of seconds to wait before failing</param>
+        /// <param name="assertion">String to include in the thrown exception</param>
+        /// <exception cref="ArgumentNullException">if condition or assertion are null</exception>
+        /// <exception cref="TrueException">
+        /// If <paramref name="condition"/> does not become true within <paramref name="seconds"/>
+        /// </exception>
+        public static Task WaitForCondition(Func<bool> condition, int seconds,
+            string assertion = "Expected condition to trigger")
+        {
+            return WaitForCondition(() => Task.FromResult(condition()), seconds, () => assertion);
+        }
+
+        /// <summary>
+        /// Returns a task that polls a method every 200 milliseconds and returns once it
+        /// is true. This throws an exception if the condition does not become true within <paramref name="seconds"/>.
+        /// </summary>
+        /// <param name="condition">Method to poll</param>
+        /// <param name="seconds">Number of seconds to wait before failing</param>
+        /// <param name="assertion">Method returning a string to include in the thrown exception</param>
+        /// <exception cref="ArgumentNullException">if condition or assertion are null</exception>
+        /// <exception cref="TrueException">
+        /// If <paramref name="condition"/> does not become true within <paramref name="seconds"/>
+        /// </exception>
+        public static Task WaitForCondition(Func<bool> condition, int seconds,
+            Func<string> assertion)
+        {
+            return WaitForCondition(() => Task.FromResult(condition()), seconds, assertion);
+        }
+
+        /// <summary>
+        /// Returns a task that polls an asynchronous method every 200 milliseconds and returns once it
+        /// is true. This throws an exception if the condition does not become true within <paramref name="seconds"/>.
+        /// </summary>
+        /// <param name="condition">Method to poll</param>
+        /// <param name="seconds">Number of seconds to wait before failing</param>
+        /// <param name="assertion">String to include in the thrown exception</param>
+        /// <exception cref="ArgumentNullException">if condition or assertion are null</exception>
+        /// <exception cref="TrueException">
+        /// If <paramref name="condition"/> does not become true within <paramref name="seconds"/>
+        /// </exception>
+        public static Task WaitForCondition(Func<Task<bool>> condition, int seconds,
+            string assertion = "Expected condition to trigger")
+        {
+            return WaitForCondition(condition, seconds, () => assertion);
+        }
+    }
+}
