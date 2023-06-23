79287382bea5537f71a1ede2cd90150a81ab5405
Ozan Göktan <72358629+ozangoktan@users.noreply.github.com>
2023-06-23 12:26:10 +0200

    Job Failure Threshold (#265)
    * add class for managing failure threshold
    
    * make cancellation token source protected
    
    * Update Cognite.Common/FailureThresholdManager.cs
    
    Co-authored-by: Einar <einar@omang.com>
    
    * convert token source to property
    
    * switch to custom callback action
    
    * return failed jobs on callback
    
    * skip creating hashset of dictionary keys
    
    * add tests
    
    ---------
    
    Co-authored-by: Einar <einar@omang.com>

---
+++ Cognite.Common/FailureThresholdManager.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cognite.Extractor.Common
+{
+    /// <summary>
+    /// Simple threshold manager, tracks failed jobs and cancels execution if budget is exhausted
+    /// </summary>
+    public class FailureThresholdManager<T> where T : IComparable
+    {
+        private ConcurrentDictionary<T, byte> _failedJobs = new ConcurrentDictionary<T, byte>();
+
+        /// <summary>
+        /// Get failed jobs
+        /// </summary>
+        public IEnumerable<T> FailedJobs => _failedJobs.Keys;
+
+        private double _thresholdPercentage;
+
+        /// <summary>
+        /// Threshold for failed jobs, %**,*
+        /// </summary>
+        public double ThresholdPercentage
+        {
+            get { return _thresholdPercentage; }
+            private set
+            {
+                if (_thresholdPercentage > 100 || _thresholdPercentage < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                else
+                {
+                    _thresholdPercentage = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Total number of jobs
+        /// </summary>
+        public long TotalJobCount { get; private set; }
+
+        private double FailureBudget => TotalJobCount * ThresholdPercentage / 100;
+
+        /// <summary>
+        /// Remaining budget for failed jobs
+        /// </summary>
+        public long RemainingBudget { get { return (long)Math.Floor(FailureBudget - _failedJobs.Count); } }
+        private readonly Action<IEnumerable<T>> _callback;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="thresholdPercentage">Threshold for failed jobs, %**,*</param>
+        /// <param name="totalJobCount">Total number of jobs</param>
+        /// <param name="callback">Callback method for when the threshold is exceeded</param>
+        public FailureThresholdManager(double thresholdPercentage, long totalJobCount, Action<IEnumerable<T>> callback)
+        {
+
+            ThresholdPercentage = thresholdPercentage;
+            TotalJobCount = totalJobCount;
+            _callback = callback;
+        }
+
+        /// <summary>
+        /// Adds job to failed items, checks if the failure budget has been exhausted
+        /// </summary>
+        /// <param name="job">Threshold for failed jobs, %**,*</param>
+        public void Failed(T job)
+        {
+            _failedJobs[job] = 0;
+            _checkThreshold();
+        }
+
+        /// <summary>
+        /// Updates parameters for calculating the failure budget
+        /// </summary>
+        /// <param name="thresholdPercentage">Threshold for failed jobs, %**,*</param>
+        /// <param name="totalJobCount">Total number of jobs</param>
+        /// <param name="validate">Check if the updated failure budget is already exhausted</param>
+        public void UpdateBudget(double thresholdPercentage, long totalJobCount, bool validate = true)
+        {
+            ThresholdPercentage = thresholdPercentage;
+            TotalJobCount = totalJobCount;
+            if (validate)
+                _checkThreshold();
+        }
+
+        private void _checkThreshold()
+        {
+            if (_failedJobs.Count > FailureBudget)
+            {
+                _callback(FailedJobs);
+            }
+        }
+    }
+}
---
+++ ExtractorUtils.Test/unit/FailureThresholdTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Cognite.Extractor.Common;
+using Xunit;
+
+namespace ExtractorUtils.Test.Unit
+{
+    public class ThresholdTest
+    {
+        FailureThresholdManager<string> _thresholdManager;
+        IEnumerable<string> _failed;
+        public ThresholdTest()
+        {
+            _thresholdManager = new FailureThresholdManager<string>(10.1, 10, (x) => { _failed = x; });
+        }
+        [Fact]
+        public void TestThreshold()
+        {
+            _thresholdManager.Failed("a");
+            Assert.Null(_failed);
+            Assert.Equal(0, _thresholdManager.RemainingBudget);
+
+            _thresholdManager.Failed("b");
+            Assert.NotNull(_failed);
+            Assert.Contains("a", _failed);
+            Assert.Contains("b", _failed);
+        }
+        [Fact]
+        public void TestChangeThresholdTrigger()
+        {
+            _thresholdManager.Failed("c");
+            Assert.Null(_failed);
+            Assert.Equal(0, _thresholdManager.RemainingBudget);
+
+            _thresholdManager.UpdateBudget(20.1, 30);
+            Assert.Equal(5, _thresholdManager.RemainingBudget);
+
+            _thresholdManager.UpdateBudget(9, 10);
+            Assert.NotNull(_failed);
+            Assert.Contains("c", _failed);
+        }
+    }
+}
