3691432f37f8920bcdd7b31db1ed744932590a74
Einar Omang <einar.omang@cognite.com>
2023-11-16 12:13:45 +0100

    Add option to set blocking resource counter capacity

--- Cognite.Common/BlockingResourceCounter.cs
+++ Cognite.Common/BlockingResourceCounter.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -43,6 +41,11 @@ namespace Cognite.Extractor.Common
         /// </summary>
         public int Count { get; private set; }
 
+        /// <summary>
+        /// Reference to the total capacity of the counter.
+        /// </summary>
+        public int Capacity { get; private set; }
+
         private readonly object _lock = new object();
         /// <summary>
         /// Resource counter constructor.
@@ -51,16 +54,19 @@ namespace Cognite.Extractor.Common
         public BlockingResourceCounter(int initial)
         {
             Count = initial;
+            Capacity = initial;
         }
 
         /// <inheritdocs />
         public Task<int> Take(int count, bool shouldBlock)
         {
+            if (count < 0) throw new ArgumentException("count must be greater than or equal to 0");
+            if (count == 0) return Task.FromResult(0);
             return Task.Run(() =>
             {
                 lock (_lock)
                 {
-                    while (Count == 0 && shouldBlock)
+                    while (Count <= 0 && shouldBlock)
                     {
                         Monitor.Wait(_lock);
                     }
@@ -72,13 +78,34 @@ namespace Cognite.Extractor.Common
             });
         }
 
+        /// <summary>
+        /// Set the capacity of the counter, may set the current count to less than 0.
+        /// </summary>
+        /// <param name="newCapacity">New capacity</param>
+        /// <exception cref="ArgumentException">If <paramref name="newCapacity"/> is less than 0</exception>
+        public void SetCapacity(int newCapacity)
+        {
+            if (newCapacity < 0) throw new ArgumentException("newCapacity must be greater than or equal to 0");
+            lock (_lock)
+            {
+                // Modify the count by the diff in capacity. If the capacity increases,
+                // increment the number of available resources.
+                var diff = newCapacity - Capacity;
+                Capacity = newCapacity;
+                Count += diff;
+
+                Monitor.PulseAll(_lock);
+            }
+        }
+
         /// <inheritdocs />
         public void Free(int count)
         {
+            if (count < 0) throw new ArgumentException("count must be greater than or equal to 0");
             lock (_lock)
             {
                 Count += count;
-                Monitor.Pulse(_lock);
+                Monitor.PulseAll(_lock);
             }
         }
     }
