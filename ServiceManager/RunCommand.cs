9c081026afbefdc4109229c20a4590cc3b7ce239
Einar Marstrander Omang <einar@omang.com>
2024-02-13 09:00:09 +0100

    Service manager prototype

---
+++ ServiceManager/RunCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace ServiceManager
+{
+    internal class RunCommand
+    {
+        /// <summary>
+        /// Runs a command in cmd.exe - timeout will occur after 1 minute
+        /// </summary>
+        /// <param name="argument"></param>
+        /// <returns>stdout of the command</returns>
+        public static string Run(string argument)
+        {
+            ProcessStartInfo pinfo = new ProcessStartInfo();
+            pinfo.FileName = "cmd.exe";
+            pinfo.Arguments = argument;
+            pinfo.UseShellExecute = false;
+            pinfo.RedirectStandardOutput = true;
+            pinfo.CreateNoWindow = true;
+            string result = string.Empty;
+
+            try
+            {
+                using (Process? exeProcess = Process.Start(pinfo))
+                {
+                    if (exeProcess == null) return $"Failed to run command: {argument}";
+
+                    using (StreamReader reader = exeProcess.StandardOutput)
+                    {
+                        result = reader.ReadToEnd();
+                    }
+                    // Wait 1 minute for the command to finish
+                    exeProcess.WaitForExit(1000 * 60 * 1);
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+            return result;
+
+        }
+    }
+}
