65fb708ac25b73f8273f388fb6ed2127b2624d7f
Einar Omang <einar.omang@cognite.com>
2025-01-28 08:59:33 +0100

    Initial impl of task scheduler for integrations

---
+++ ExtractorUtils/Unstable/Tasks/ExtractorTaskScheduler.cs
@@ -0,0 +1,487 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Cognite.Extensions;
+using Cognite.Extractor.Common;
+
+namespace Cognite.ExtractorUtils.Unstable.Tasks
+{
+    /// <summary>
+    /// Interface implemented by tasks schedulable by the BaseTaskScheduler.
+    /// </summary>
+    public interface ISchedulableTask
+    {
+        /// <summary>
+        /// Run the task, this may throw an exception which is treated as a fatal error.
+        /// </summary>
+        /// <param name="task">Callbacks for reporting errors.</param>
+        /// <param name="token">Optional cancellation token.</param>
+        public Task Run(BaseErrorReporter task, CancellationToken token);
+        /// <summary>
+        /// Return whether the task can run now.
+        /// 
+        /// The task should make sure to call the callback provided in `RegisterReadyCallback`
+        /// once this will return `true`.
+        /// 
+        /// Once this returns `true`, the task may be started immediately.
+        /// 
+        /// Note that this may be called frequently, and should not do any expensive calculations.
+        /// </summary>
+        public bool CanRunNow();
+        /// <summary>
+        /// Register a callback that should be invoked once `CanRunNow` returns `true`.
+        /// 
+        /// You may call this callback even if `CanRunNow` isn't guaranteed to return `true`.
+        /// </summary>
+        /// <param name="callback"></param>
+        public void RegisterReadyCallback(Action callback);
+
+        /// <summary>
+        /// Return whether this task failing should be considered fatal
+        /// and terminate the task scheduler completely.
+        /// </summary>
+        public bool ErrorIsFatal { get; }
+    }
+
+    /// <summary>
+    /// Base class for schedulable tasks, just implements
+    /// RegisterReadyCallback and leaves the rest of the
+    /// ISchedulableTask interface abstract.
+    /// </summary>
+    public abstract class BaseSchedulableTask : ISchedulableTask
+    {
+        /// <inheritdoc />
+        public abstract bool ErrorIsFatal { get; }
+
+        /// <inheritdoc />
+        public abstract bool CanRunNow();
+
+        /// <summary>
+        /// Callback registered on task startup, should be called
+        /// to notify the task runner that this task is now ready to run.
+        /// </summary>
+        protected Action? ReadyCallback { get; private set; }
+
+        /// <inheritdoc />
+        public virtual void RegisterReadyCallback(Action callback)
+        {
+            ReadyCallback = callback;
+        }
+
+        /// <inheritdoc />
+        public abstract Task Run(BaseErrorReporter task, CancellationToken token);
+    }
+
+    internal sealed class RunningTaskInfo : IDisposable
+    {
+        public Task Task { get; }
+        public CancellationTokenSource Source { get; }
+
+        public RunningTaskInfo(Task activeTask, CancellationTokenSource tokenSource)
+        {
+            Source = tokenSource;
+            Task = activeTask;
+        }
+
+        public void Dispose()
+        {
+            Source.Dispose();
+        }
+    }
+
+    internal sealed class RegisteredTask : IDisposable
+    {
+        public ITimeSpanProvider? Schedule { get; }
+        public DateTime? NextRun { get; set; }
+        public ISchedulableTask Operation { get; }
+
+        public RunningTaskInfo? ActiveTask { get; private set; }
+
+        private List<TaskCompletionSource<Exception?>> _waiters = new List<TaskCompletionSource<Exception?>>();
+
+        private object _lock = new object();
+        private TaskReporter _reporter;
+
+        public RegisteredTask(ITimeSpanProvider? schedule, ISchedulableTask operation, TaskReporter reporter, bool runImmediately)
+        {
+            Operation = operation;
+            Schedule = schedule;
+            _reporter = reporter;
+            if (runImmediately)
+            {
+                NextRun = DateTime.UtcNow;
+            }
+            else if (schedule != null)
+            {
+                NextRun = DateTime.UtcNow + schedule.Value;
+            }
+        }
+
+        public void Run(DateTime now, CancellationToken token)
+        {
+            if (ActiveTask != null) throw new InvalidOperationException("Attempt to start an already running task");
+            var source = CancellationTokenSource.CreateLinkedTokenSource(token);
+            var task = Task.Run(() => Operation.Run(_reporter, source.Token), source.Token);
+
+            if (Schedule != null)
+            {
+                NextRun = now + Schedule.Value;
+            }
+            else
+            {
+                NextRun = null;
+            }
+
+            ActiveTask = new RunningTaskInfo(task, source);
+            _reporter.ReportStart(now);
+        }
+
+        public void Cancel()
+        {
+            lock (_lock)
+            {
+                if (ActiveTask != null)
+                {
+                    ActiveTask.Source.Cancel();
+                    foreach (var waiter in _waiters)
+                    {
+                        waiter.TrySetCanceled();
+                    }
+                    _waiters.Clear();
+                }
+            }
+        }
+
+        public void AddWaiter(TaskCompletionSource<Exception?> waiter)
+        {
+            lock (_lock)
+            {
+                _waiters.Add(waiter);
+            }
+        }
+
+        public void FinishTask(DateTime now)
+        {
+            if (ActiveTask == null || !ActiveTask.Task.IsCompleted) throw new InvalidOperationException("Attempt to finish a task that isn't completed");
+            lock (_lock)
+            {
+                var finished = ActiveTask;
+                finished.Dispose();
+                ActiveTask = null;
+
+                Exception? exc = finished.Task.Exception?.Flatten();
+
+                if (exc?.InnerException != null)
+                {
+                    exc = exc.InnerException;
+                }
+
+                // Report a fatal error to integrations if the task exited non-cleanly.
+                // This typically means a crash or manual cancellation.
+                if (finished.Task.IsCanceled)
+                {
+                    _reporter.Fatal("Task was cancelled", null, now);
+                    exc = new TaskCanceledException();
+                }
+                else if (exc != null)
+                {
+                    _reporter.Fatal(exc.Message, exc.StackTrace?.ToString(), now);
+                }
+                else if (finished.Task.IsFaulted)
+                {
+                    // Should be impossible.
+                    _reporter.Fatal("Task failed without throwing an exception.", null, now);
+                    exc = new CogniteUtilsException("Task failed without throwing an exception");
+                }
+
+                // Report that the task ended.
+                _reporter.ReportEnd(now);
+
+                // Wake up any waiters and tell them the task has finished running.
+                foreach (var cb in _waiters)
+                {
+                    cb.TrySetResult(exc);
+                }
+                _waiters.Clear();
+
+                // If the task is critical, then at this stage we should throw an exception.
+                if (exc != null && Operation.ErrorIsFatal)
+                {
+                    ExceptionDispatchInfo.Capture(exc).Throw();
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            lock (_lock)
+            {
+                ActiveTask?.Dispose();
+                foreach (var waiter in _waiters)
+                {
+                    waiter.TrySetCanceled();
+                }
+                _waiters.Clear();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Task scheduler that runs tasks and reports their status to the integrations API.
+    /// </summary>
+    public class ExtractorTaskScheduler : IDisposable
+    {
+        // The token source must be initialized when you call `Run`.
+        private CancellationTokenSource? _source;
+        private readonly IIntegrationSink _sink;
+        private readonly Dictionary<string, RegisteredTask> _tasks = new Dictionary<string, RegisteredTask>();
+
+
+        private object _lock = new object();
+        private ManualResetEvent _evt = new ManualResetEvent(false);
+
+        private bool disposedValue;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="sink">Sink for task updates.</param>
+        public ExtractorTaskScheduler(IIntegrationSink sink)
+        {
+            _sink = sink;
+        }
+
+        /// <summary>
+        /// Wake up the scheduler and tell it to check its tasks again.
+        /// </summary>
+        public void Notify()
+        {
+            _evt.Set();
+        }
+
+        /// <summary>
+        /// Add a task to the scheduler.
+        /// </summary>
+        /// <param name="name">Name of task to add, this must be unique.</param>
+        /// <param name="schedule">Optional schedule to run the task on. Can be null
+        /// in which case the task will only be triggered manually.</param>
+        /// <param name="operation">Type implementing the runnable task.</param>
+        /// <param name="runImmediately">Whether to run the task immediately.</param>
+        /// <exception cref="InvalidOperationException">If a task with the same name already exists.</exception>
+        public void AddScheduledTask(string name, ITimeSpanProvider? schedule, ISchedulableTask operation, bool runImmediately)
+        {
+            if (operation == null) throw new ArgumentNullException(nameof(operation));
+            if (name == null) throw new ArgumentNullException(nameof(name));
+
+            lock (_lock)
+            {
+                if (_tasks.ContainsKey(name))
+                {
+                    throw new InvalidOperationException($"Task {name} already exists");
+                }
+
+                operation.RegisterReadyCallback(Notify);
+                var task = new RegisteredTask(schedule, operation, new TaskReporter(name, _sink), runImmediately);
+                _tasks.Add(name, task);
+                _evt.Set();
+            }
+        }
+
+        /// <summary>
+        /// Cancel a task if it is currently running.
+        /// </summary>
+        /// <param name="name">Name of the task to cancel</param>
+        public void CancelTask(string name)
+        {
+            if (name == null) throw new ArgumentNullException(nameof(name));
+
+            lock (_lock)
+            {
+                if (!_tasks.TryGetValue(name, out var task))
+                {
+                    throw new InvalidOperationException($"No task with name {name}");
+                }
+                task.Cancel();
+            }
+        }
+
+        /// <summary>
+        /// Schedule a task now. If it is already running it will 
+        /// </summary>
+        /// <param name="name">Name of the task to schedule</param>
+        /// <param name="reScheduleIfRunning">If true, re-schedule the
+        /// task to run again once it finishes. If false, and the task is already
+        /// running, nothing will happen.</param>
+        public void ScheduleTaskNow(string name, bool reScheduleIfRunning = false)
+        {
+            if (name == null) throw new ArgumentNullException(nameof(name));
+
+            lock (_lock)
+            {
+                if (!_tasks.TryGetValue(name, out var task))
+                {
+                    throw new InvalidOperationException($"No task with name {name}");
+                }
+                if (reScheduleIfRunning || task.ActiveTask == null)
+                {
+                    task.NextRun = DateTime.UtcNow;
+                    _evt.Set();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Wait for the next time the task given by <paramref name="task"/> ends.
+        /// 
+        /// If the task is not currently running, this will wait until it starts running and
+        /// then ends.
+        /// 
+        /// Note that if the task fails, this will re-throw the exception that caused
+        /// the task failure, or a TaskCanceledException if it was canceled.
+        /// </summary>
+        /// <param name="task">Task to wait for, must be added to the scheduler.</param>
+        /// <param name="timeout">Timeout, set to Timeout.InfiniteTimespan to wait forever.</param>
+        /// <param name="token">Optional cancellation token.</param>
+        /// <exception cref="ArgumentException">If the task does not exist.</exception>
+        /// <exception cref="TimeoutException">If waiting for the task to end timed out.</exception>
+        public async Task WaitForNextEndOfTask(string task, TimeSpan timeout, CancellationToken token = default)
+        {
+            var evt = new TaskCompletionSource<Exception?>();
+            lock (_lock)
+            {
+                if (!_tasks.TryGetValue(task, out var t))
+                {
+                    throw new ArgumentException($"Task with name {task} does not exist");
+                }
+                t.AddWaiter(evt);
+            }
+
+            var delay = Task.Delay(timeout, token);
+            var r = await Task.WhenAny(evt.Task, delay).ConfigureAwait(false);
+            token.ThrowIfCancellationRequested();
+            if (r == delay)
+            {
+                throw new TimeoutException($"Waiting for end of task {task} timed out after {timeout}");
+            }
+            else
+            {
+                var exc = evt.Task.Result;
+                if (exc != null)
+                {
+                    ExceptionDispatchInfo.Capture(exc).Throw();
+                }
+            }
+        }
+
+        private bool _started;
+
+        /// <summary>
+        /// Run the scheduler.
+        /// 
+        /// This should only be called once on a given scheduler.
+        /// 
+        /// To re-run it after a crash, the scheduler must be re-initialized.
+        /// </summary>
+        /// <param name="token">Global cancellation token for stopping the entire scheduler.</param>
+        public async Task Run(CancellationToken token)
+        {
+            if (_started) throw new InvalidOperationException("Attempt to run scheduler multiple times");
+            _started = true;
+            _source = CancellationTokenSource.CreateLinkedTokenSource(token);
+            while (!_source.IsCancellationRequested)
+            {
+                _evt.Reset();
+                var tickTime = DateTime.UtcNow;
+                var toAwait = new List<Task>();
+
+                lock (_lock)
+                {
+
+                    DateTime? minNextRun = null;
+                    var tasksToWaitFor = new List<Task>();
+                    foreach (var task in _tasks.Values)
+                    {
+                        // If the task has finished, take steps to mark it as completed.
+                        if (task.ActiveTask != null && task.ActiveTask.Task.IsCompleted)
+                        {
+                            task.FinishTask(tickTime);
+                        }
+
+                        // Start the task again if it is now not running but it is scheduled to run.
+                        if (task.NextRun.HasValue
+                            && task.ActiveTask == null
+                            && task.Operation.CanRunNow())
+                        {
+                            if (task.NextRun.Value <= tickTime)
+                            {
+                                task.Run(tickTime, token);
+                            }
+                            else if (minNextRun == null || minNextRun > task.NextRun.Value)
+                            {
+                                minNextRun = task.NextRun.Value;
+                            }
+                        }
+
+                        // If the task is now running, add it to the list of things we are going to listen to.
+                        if (task.ActiveTask != null)
+                        {
+                            tasksToWaitFor.Add(task.ActiveTask.Task);
+                        }
+                    }
+
+                    // If there is a task that is going to run in the future, add a task to wait for that time.
+                    if (minNextRun != null)
+                    {
+#pragma warning disable CA2016 // Forward the 'CancellationToken' parameter to methods
+                        toAwait.Add(Task.Delay(tickTime - minNextRun.Value));
+#pragma warning restore CA2016 // Forward the 'CancellationToken' parameter to methods
+                    }
+                    if (tasksToWaitFor.Count > 0)
+                    {
+                        toAwait.Add(Task.WhenAny(tasksToWaitFor));
+                    }
+                }
+
+                // Always wait for the event to trigger.
+                toAwait.Add(CommonUtils.WaitAsync(_evt, Timeout.InfiniteTimeSpan, _source.Token));
+
+                await Task.WhenAny(toAwait).ConfigureAwait(false);
+            }
+        }
+
+        /// <summary>
+        /// Dispose of the scheduler.
+        /// 
+        /// Can be overridden in base classes.
+        /// </summary>
+        /// <param name="disposing">Whether to dispose managed resources.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _source?.Cancel();
+                    _source?.Dispose();
+                    _evt.Dispose();
+                    foreach (var task in _tasks)
+                    {
+                        task.Value.Dispose();
+                    }
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ ExtractorUtils/Unstable/Tasks/Task.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Threading;
+using CogniteSdk.Alpha;
+
+namespace Cognite.ExtractorUtils.Unstable.Tasks
+{
+    /// <summary>
+    /// Type handling reporting of events related to a single task.
+    /// </summary>
+    public class TaskReporter : BaseErrorReporter
+    {
+        private string TaskName { get; }
+
+        private IIntegrationSink _sink;
+
+        // Current state, for sanity checking.
+        private bool _isRunning;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="taskName">Name of the task, must be unique.</param>
+        /// <param name="sink">Sink for task events.</param>
+        public TaskReporter(string taskName, IIntegrationSink sink)
+        {
+            TaskName = taskName;
+            _sink = sink;
+        }
+
+        /// <inheritdoc />
+        public override ExtractorError NewError(ErrorLevel level, string description, string? details = null, DateTime? now = null)
+        {
+            return new ExtractorError(level, description, _sink, details, TaskName, now);
+        }
+
+        /// <summary>
+        /// Report that this task has started.
+        /// </summary>
+        /// <param name="timestamp"></param>
+        public void ReportStart(DateTime? timestamp = null)
+        {
+            if (_isRunning) throw new InvalidOperationException("Attempted to start task that is already running");
+            _isRunning = true;
+            _sink.ReportTaskStart(TaskName, timestamp);
+        }
+
+        /// <summary>
+        /// Report that this task has ended.
+        /// </summary>
+        /// <param name="timestamp"></param>
+        public void ReportEnd(DateTime? timestamp = null)
+        {
+            if (!_isRunning) throw new InvalidOperationException("Attempted to end task that is not running");
+            _isRunning = false;
+            _sink.ReportTaskEnd(TaskName, timestamp);
+        }
+    }
+}
