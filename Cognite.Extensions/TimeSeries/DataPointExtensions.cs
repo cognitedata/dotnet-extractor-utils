830f5dd2169d14c82a91b5a3698f67339badc5ae
Toshad Salwekar <toshadsalwekar@gmail.com>
2026-08-05 19:05:25 +0530

    gemini

--- Cognite.Extensions/TimeSeries/DataPointExtensions.cs
+++ Cognite.Extensions/TimeSeries/DataPointExtensions.cs
@@ -97,7 +97,7 @@ namespace Cognite.Extensions
                                 Code = (long)dp.Status.Code
                             }
                         };
-                        if (dp.NumericValue.HasValue)
+                        if (dp.NumericValue.HasValue && double.IsFinite(dp.NumericValue.Value))
                         {
                             stateDp.NumericValue = (long)Math.Round(dp.NumericValue.Value);
                         }
